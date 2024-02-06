e68b898f501387375e201408c45b2048b43850cb
Kent Mossbäck <kent.mossback@scb.se>
2024-02-06 16:04:13 +0100

    Added Ingress component to app.tsx

--- libs/pxweb2-ui/src/lib/components/Typography/Ingress/Ingress.stories.tsx
+++ libs/pxweb2-ui/src/lib/components/Typography/Ingress/Ingress.stories.tsx
@@ -9,6 +9,6 @@ export default meta;
 
 export const Primary = {
   args: {
-    children: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    children: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
   },
 };
