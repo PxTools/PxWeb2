295ff503aa0e7338f0f6b020040d2e93ba6cfb96
PerIngeVaaje <per.inge.vaaje@ssb.no>
2024-02-08 13:53:23 +0100

    PXWEB-88 Format  and added margin, moved some common used element

--- libs/pxweb2-ui/src/lib/components/Typography/BodyLong/BodyLong.tsx
+++ libs/pxweb2-ui/src/lib/components/Typography/BodyLong/BodyLong.tsx
@@ -1,16 +1,29 @@
 import cl from 'clsx';
 import classes from './BodyLong.module.scss';
 
-export interface BodyLongProps extends React.HTMLAttributes<HTMLParagraphElement>  {
-  size?:"small"|"medium",
-  spacing?:boolean,
-  children:React.ReactNode;
+export interface BodyLongProps
+  extends React.HTMLAttributes<HTMLParagraphElement> {
+  size?: 'small' | 'medium';
+  spacing?: boolean;
+  children: React.ReactNode;
 }
 
-export function BodyLong({children,size="medium",spacing=false,...rest}: BodyLongProps) {
+export function BodyLong({
+  children,
+  size = 'medium',
+  spacing = false,
+  ...rest
+}: BodyLongProps) {
   return (
-    <p className={cl(classes.bodylong,classes[size],cl({[classes[`${size}-spacing`]]:spacing}))}{...rest}>  
-     {children}
+    <p
+      className={cl(
+        classes.bodylong,
+        classes[size],
+        cl({ [classes[`${size}-spacing`]]: spacing })
+      )}
+      {...rest}
+    >
+      {children}
     </p>
   );
 }
--- libs/pxweb2-ui/src/lib/components/Typography/BodyLong/bodyLong.stories.tsx
+++ libs/pxweb2-ui/src/lib/components/Typography/BodyLong/bodyLong.stories.tsx
@@ -1,6 +1,5 @@
-import type { Meta, StoryObj,StoryFn } from '@storybook/react';
+import type { Meta, StoryObj, StoryFn } from '@storybook/react';
 import { BodyLong } from './BodyLong';
-
 
 const meta: Meta<typeof BodyLong> = {
   component: BodyLong,
@@ -9,67 +8,90 @@ const meta: Meta<typeof BodyLong> = {
 export default meta;
 type Story = StoryObj<typeof BodyLong>;
 
-export const Variant:Story = {
-  args: {children:"This is a story about Little Red Ridinghood. One day she went into the wood to visit her grandmother. The day after too, She visited her every day, every week, every month, every year. She never saw a wolf, no even a little fox. ",
-size:"small"}
+export const Variant: Story = {
+  args: {
+    children:
+      'This is a story about Little Red Ridinghood. One day she went into the wood to visit her grandmother. The day after too, She visited her every day, every week, every month, every year. She never saw a wolf, no even a little fox. ',
+    size: 'small',
+  },
 };
 
-
-
-export const MediumWithSpacing:StoryFn<typeof BodyLong> =()=> {
-  return(
-<>
-    <BodyLong size='medium' spacing >
-    This is a story about Little Red Ridinghood. One day she went into the wood to visit her grandmother. The day after too, She visited her every day, every week, every month, every year. She never saw a wolf, no even a little fox. 
-    </BodyLong>
-  <BodyLong size='medium' spacing>
-  This is a story about Little Red Ridinghood. One day she went into the wood to visit her grandmother. The day after too, She visited her every day, every week, every month, every year. She never saw a wolf, no even a little fox. 
-    </BodyLong>
-    <hr></hr>
-</>
-  )
+export const MediumWithSpacing: StoryFn<typeof BodyLong> = () => {
+  return (
+    <>
+      <BodyLong size="medium" spacing>
+        This is a story about Little Red Ridinghood. One day she went into the
+        wood to visit her grandmother. The day after too, She visited her every
+        day, every week, every month, every year. She never saw a wolf, no even
+        a little fox.
+      </BodyLong>
+      <BodyLong size="medium" spacing>
+        This is a story about Little Red Ridinghood. One day she went into the
+        wood to visit her grandmother. The day after too, She visited her every
+        day, every week, every month, every year. She never saw a wolf, no even
+        a little fox.
+      </BodyLong>
+      <hr></hr>
+    </>
+  );
 };
 
-export const MediumWithoutSpacing:StoryFn<typeof BodyLong> =()=> {
-  return(
-<>
-    <BodyLong size='medium'  >
-    This is a story about Little Red Ridinghood. One day she went into the wood to visit her grandmother. The day after too, She visited her every day, every week, every month, every year. She never saw a wolf, no even a little fox. 
-    </BodyLong>
-  <BodyLong size='medium' >
-  This is a story about Little Red Ridinghood. One day she went into the wood to visit her grandmother. The day after too, She visited her every day, every week, every month, every year. She never saw a wolf, no even a little fox. 
-    </BodyLong>
-    <hr></hr>
-</>
-  )
-}
-
-export const SmallWithSpacing:StoryFn<typeof BodyLong> =()=> {
-  return(
-<>
-    <BodyLong size='small' spacing >
-    This is a story about Little Red Ridinghood. One day she went into the wood to visit her grandmother. The day after too, She visited her every day, every week, every month, every year. She never saw a wolf, no even a little fox. 
-    </BodyLong>
-  <BodyLong size='small' spacing>
-  This is a story about Little Red Ridinghood. One day she went into the wood to visit her grandmother. The day after too, She visited her every day, every week, every month, every year. She never saw a wolf, no even a little fox. 
-    </BodyLong>
-    <hr></hr>
-</>
-  )
+export const MediumWithoutSpacing: StoryFn<typeof BodyLong> = () => {
+  return (
+    <>
+      <BodyLong size="medium">
+        This is a story about Little Red Ridinghood. One day she went into the
+        wood to visit her grandmother. The day after too, She visited her every
+        day, every week, every month, every year. She never saw a wolf, no even
+        a little fox.
+      </BodyLong>
+      <BodyLong size="medium">
+        This is a story about Little Red Ridinghood. One day she went into the
+        wood to visit her grandmother. The day after too, She visited her every
+        day, every week, every month, every year. She never saw a wolf, no even
+        a little fox.
+      </BodyLong>
+      <hr></hr>
+    </>
+  );
 };
 
-export const SmallWithoutSpacing:StoryFn<typeof BodyLong> =()=> {
-  return(
-<>
-    <BodyLong size='small'  >
-    This is a story about Little Red Ridinghood. One day she went into the wood to visit her grandmother. The day after too, She visited her every day, every week, every month, every year. She never saw a wolf, no even a little fox. 
-    </BodyLong>
-  <BodyLong size='small' >
-  This is a story about Little Red Ridinghood. One day she went into the wood to visit her grandmother. The day after too, She visited her every day, every week, every month, every year. She never saw a wolf, no even a little fox. 
-    </BodyLong>
-    <hr></hr>
-</>
-  )
-}
+export const SmallWithSpacing: StoryFn<typeof BodyLong> = () => {
+  return (
+    <>
+      <BodyLong size="small" spacing>
+        This is a story about Little Red Ridinghood. One day she went into the
+        wood to visit her grandmother. The day after too, She visited her every
+        day, every week, every month, every year. She never saw a wolf, no even
+        a little fox.
+      </BodyLong>
+      <BodyLong size="small" spacing>
+        This is a story about Little Red Ridinghood. One day she went into the
+        wood to visit her grandmother. The day after too, She visited her every
+        day, every week, every month, every year. She never saw a wolf, no even
+        a little fox.
+      </BodyLong>
+      <hr></hr>
+    </>
+  );
+};
 
-
+export const SmallWithoutSpacing: StoryFn<typeof BodyLong> = () => {
+  return (
+    <>
+      <BodyLong size="small">
+        This is a story about Little Red Ridinghood. One day she went into the
+        wood to visit her grandmother. The day after too, She visited her every
+        day, every week, every month, every year. She never saw a wolf, no even
+        a little fox.
+      </BodyLong>
+      <BodyLong size="small">
+        This is a story about Little Red Ridinghood. One day she went into the
+        wood to visit her grandmother. The day after too, She visited her every
+        day, every week, every month, every year. She never saw a wolf, no even
+        a little fox.
+      </BodyLong>
+      <hr></hr>
+    </>
+  );
+};
