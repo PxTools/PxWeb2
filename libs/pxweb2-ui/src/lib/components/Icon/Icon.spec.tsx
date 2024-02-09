fc001c79529026e158778c171eb0b385361668b6
Michael Pande <git@e2e.no>
2024-02-09 13:33:19 +0100

    Added all icons and expanded icon component to support them. Also made sure typings for icons work as intended and added a CSS variable util class

--- libs/pxweb2-ui/src/lib/components/Icon/Icon.spec.tsx
+++ libs/pxweb2-ui/src/lib/components/Icon/Icon.spec.tsx
@@ -1,10 +1,10 @@
 import { render } from '@testing-library/react';
 
-import Icon from './Icon';
+import { Icon } from './Icon';
 
 describe('Icon', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Icon />);
+    const { baseElement } = render(<Icon iconName="ChevronDown" />);
     expect(baseElement).toBeTruthy();
   });
 });
