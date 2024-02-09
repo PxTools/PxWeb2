9ef85d5e6f299a3fdf0e992d036fe59bbbf599db
PerIngeVaaje <per.inge.vaaje@ssb.no>
2024-02-09 13:14:08 +0100

    PXWEB-86 Added option for align and text color.  Change order for size-option in Story.  Show level option as radiobutton in story.

--- libs/pxweb2-ui/src/lib/components/Typography/Heading/Heading.stories.tsx
+++ libs/pxweb2-ui/src/lib/components/Typography/Heading/Heading.stories.tsx
@@ -7,67 +7,75 @@ const meta: Meta<typeof Heading> = {
   argTypes: {
     as: {
       table: {
-        disable: true
-      }
-    }
-  }
+        disable: true,
+      },
+    },
+  },
 };
 export default meta;
 type Story = StoryObj<typeof Heading>;
 
-const text = "Heading is used for headlines to describe the content"
+const text = 'Heading is used for headlines to describe the content';
 
 export const Default: Story = {
   args: {
     children: text,
   },
+  argTypes: {
+    size: {
+      options: ['xlarge', 'large', 'medium', 'small', 'xsmall'],
+      control: { type: 'radio' },
+    },
+    level: {
+      control: { type: 'radio' },
+    },
+  },
 };
 
 export const HeadingXLarge: Story = {
-    args: {
-      size: 'xlarge',
-      children: text
-    },
-  };
+  args: {
+    size: 'xlarge',
+    children: text,
+  },
+};
 
-  export const HeadingLarge: Story = {
-    args: {
-      size: 'large',
-      children: text
-    },
-  };
+export const HeadingLarge: Story = {
+  args: {
+    size: 'large',
+    children: text,
+  },
+};
 
-  export const HeadingMedium: Story = {
-    args: {
-      size: 'medium',
-      children: text
-    },
-  };
+export const HeadingMedium: Story = {
+  args: {
+    size: 'medium',
+    children: text,
+  },
+};
 
-  export const HeadingSmall: Story = {
-    args: {
-      size: 'small',
-      children: text
-    },
-  };
-  export const HeadingXSmall: Story = {
-    args: {
-      size: 'xsmall',
-      children: text
-    },
-  };
+export const HeadingSmall: Story = {
+  args: {
+    size: 'small',
+    children: text,
+  },
+};
+export const HeadingXSmall: Story = {
+  args: {
+    size: 'xsmall',
+    children: text,
+  },
+};
 
-  export const HeadingAsLegend: Story = {
-    args: {
-      children: "I am legend!",
-      as: "legend"
-    },
-  };
+export const HeadingAsLegend: Story = {
+  args: {
+    children: 'I am legend!',
+    as: 'legend',
+  },
+};
 
-  export const HeadingNoSpacing: Story = {
-    args: {
-      children: text,
-      spacing: false
-    },
-  };
-
+export const HeadingNoSpacing: Story = {
+  args: {
+    children: text,
+    spacing: false,
+  },
+};
--- libs/pxweb2-ui/src/lib/components/Typography/Heading/Heading.tsx
+++ libs/pxweb2-ui/src/lib/components/Typography/Heading/Heading.tsx
@@ -3,9 +3,11 @@ import classes from './Heading.module.scss';
 import React from 'react';
 
 /* eslint-disable-next-line */
-export interface HeadingProps extends React.HTMLAttributes<HTMLHeadingElement>{
-  size?: 'xsmall' | 'small' | 'medium' | 'large' | 'xlarge'
-  level?: "1" | "2" | "3" | "4" | "5" | "6";
+export interface HeadingProps extends React.HTMLAttributes<HTMLHeadingElement> {
+  size?: 'xsmall' | 'small' | 'medium' | 'large' | 'xlarge';
+  level?: '1' | '2' | '3' | '4' | '5' | '6';
+  align?: 'start' | 'center' | 'end';
+  color?: 'default' | 'subtle';
   spacing?: boolean;
   children: string;
   as?: React.ElementType;
@@ -14,20 +16,24 @@ export interface HeadingProps extends React.HTMLAttributes<HTMLHeadingElement>{
 export function Heading({
   size = 'medium',
   level = '1',
+  align = 'start',
+  color = 'default',
   children,
   spacing = true,
   as,
   ...rest
 }: HeadingProps) {
-   const Component = as ?? (`h${level}` as React.ElementType);
-   return (
-    <Component 
-    className={cl(
-      classes.heading,
-      classes[size],
-      {[classes.spacing]: spacing}
-    )}
-    {...rest}
+  const Component = as ?? (`h${level}` as React.ElementType);
+  return (
+    <Component
+      className={cl(
+        classes.heading,
+        classes[size],
+        { [classes.spacing]: spacing },
+        classes[`align-${align}`],
+        classes[`text-color-${color}`]
+      )}
+      {...rest}
     >
       {children}
     </Component>
