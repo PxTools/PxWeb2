c6de91d28de27e0e251735ae7a384854c6dcf54a
Kent Mossbäck <kent.mossback@scb.se>
2024-02-09 14:33:57 +0100

    Some fixes after Chromatic feedback

--- libs/pxweb2-ui/src/lib/components/Typography/Ingress/Ingress.stories.tsx
+++ libs/pxweb2-ui/src/lib/components/Typography/Ingress/Ingress.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta } from '@storybook/react';
+import type { Meta, StoryObj, StoryFn } from '@storybook/react';
 import { Ingress } from './Ingress';
 
 const meta: Meta<typeof Ingress> = {
@@ -7,8 +7,61 @@ const meta: Meta<typeof Ingress> = {
 };
 export default meta;
 
-export const Primary = {
+const text =  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+
+export const Default=  {
   args: {
-    children: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    children: text,
   },
+};
+
+export const Align: StoryFn<typeof Ingress> = () => {
+  return (
+    <>
+      <h1>Align</h1>
+
+      <h2>Default:</h2>
+      <Ingress>{text}</Ingress>
+
+      <h2>start:</h2>
+      <Ingress align='start'>{text}</Ingress>
+
+      <h2>center:</h2>
+      <Ingress align='center'>{text}</Ingress>
+
+      <h2>end:</h2>
+      <Ingress align='end'>{text}</Ingress>
+    </>
+  );
+};
+
+export const Spacing: StoryFn<typeof Ingress> = () => {
+  return (
+    <>
+      <h1>Spacing</h1>
+
+      <h2>spacing:</h2>
+      <Ingress spacing>{text}</Ingress>
+     
+      <h2>no spacing:</h2>
+      <Ingress>{text}</Ingress>      
+    </>
+  );
+};
+
+export const Textcolor: StoryFn<typeof Ingress> = () => {
+  return (
+    <>
+      <h1>Textcolor</h1>
+
+      <h2>Default:</h2>
+      <Ingress>{text}</Ingress>
+
+      <h2>default:</h2>
+      <Ingress textcolor='default'>{text}</Ingress>
+
+      <h2>subtle:</h2>
+      <Ingress textcolor='subtle'>{text}</Ingress>
+    </>
+  );
 };
--- libs/pxweb2-ui/src/lib/components/Typography/Ingress/Ingress.tsx
+++ libs/pxweb2-ui/src/lib/components/Typography/Ingress/Ingress.tsx
@@ -1,16 +1,32 @@
+import cl from 'clsx';
+import classes from './Ingress.module.scss';
 import React from 'react';
-import styles from './Ingress.module.scss';
 
-interface IngressProps {
+export interface IngressProps 
+  extends React.HTMLAttributes<HTMLParagraphElement> {
   children: React.ReactNode;  
+  spacing?: boolean;
+  align?: 'start' | 'center' | 'end';
+  textcolor?: 'default' | 'subtle';  
 }
 
-export const Ingress: React.FC<IngressProps> = ({ children }) => {
-  return (
-    <p className={styles.ingress}>
-      {children}
-    </p>
-  );
-};
+export function Ingress({
+  spacing = false,
+  align = 'start',
+  textcolor = 'default',
+  children,
+  ...rest
+}: IngressProps) { return (
+  <p
+    className={cl(
+      classes[`align-${align}`],      
+      cl({[classes[`text-color-${textcolor}`]]: textcolor})      
+    )}
+    {...rest}
+  >
+    {children}
+  </p>
+);
+}
 
 export default Ingress;
