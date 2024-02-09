a67d3fe140b84bda1eba532fca5d2f7cd4193167
Kent Mossbäck <kent.mossback@scb.se>
2024-02-09 15:24:34 +0100

    Removed unused code

--- libs/pxweb2-ui/src/lib/components/Typography/Ingress/Ingress.stories.tsx
+++ libs/pxweb2-ui/src/lib/components/Typography/Ingress/Ingress.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj, StoryFn } from '@storybook/react';
+import type { Meta, StoryFn } from '@storybook/react';
 import { Ingress } from './Ingress';
 
 const meta: Meta<typeof Ingress> = {
