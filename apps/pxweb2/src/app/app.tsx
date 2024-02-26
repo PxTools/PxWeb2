6926c53aca1e6d5da2103778afb57d9a75608327
MikaelNordberg <mikael.nordberg@scb.se>
2024-02-26 13:45:17 +0100

    Handle border. Updated border colors

--- apps/pxweb2/src/app/app.tsx
+++ apps/pxweb2/src/app/app.tsx
@@ -26,9 +26,12 @@ export function App() {
         Welcome to PxWeb 2.0
       </Heading>
       <br />
-      <Tag size="medium" variant="error">Tag</Tag>&nbsp;
-      <Tag size="small" variant="info">Tag</Tag>&nbsp;
-      <Tag size="xsmall" variant="info">Tag</Tag>
+      <Tag size="medium" variant="error" type='border'>Tag</Tag>&nbsp;
+      <Tag size="medium" variant="success">Tag</Tag>&nbsp;
+      <Tag size="small" variant="error">Tag</Tag>&nbsp;
+      <Tag size="small" variant="error" type='border'>Tag</Tag>&nbsp;
+      <Tag size="xsmall" variant="info">Tag</Tag>&nbsp;
+      <Tag size="xsmall" variant="info" type='border'>Tag</Tag>
       <Ingress spacing>
         Ingress: This page will display various components
       </Ingress>
--- libs/pxweb2-ui/src/lib/components/Tag/Tag.tsx
+++ libs/pxweb2-ui/src/lib/components/Tag/Tag.tsx
@@ -4,12 +4,14 @@ import classes from './Tag.module.scss';
 export interface TagProps extends React.HTMLAttributes<HTMLSpanElement> {
   size?: 'medium' | 'small' | 'xsmall';
   variant?: 'neutral' | 'info' | 'success'| 'warning'| 'error';
+  type?: 'default' | 'border';
   children?: React.ReactNode;
 }
 
 export function Tag({
   size = 'medium',
   variant = 'neutral',
+  type = 'default',
   children,
   ...rest
 }: TagProps) {
@@ -18,7 +20,8 @@ export function Tag({
     className={cl(
       classes.tag,
       classes[size],
-      classes[variant]
+      classes[variant],
+      classes[type] 
       )}      
       >
       {children}
