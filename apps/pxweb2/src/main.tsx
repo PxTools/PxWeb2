043ad2a3cf74503a02b50ea5a5a8877110e4cc3c
Sjur Sutterud Sagen <sjs@ssb.no>
2024-03-04 16:26:01 +0100

    Add a suspense to the main app, to show loading
    Add a basic suspense component around the App, to show loading state for
    requests the server makes.
    
    Co-authored-by: Mikael Nordberg <>

--- apps/pxweb2/src/main.tsx
+++ apps/pxweb2/src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react';
+import { StrictMode, Suspense } from 'react';
 import * as ReactDOM from 'react-dom/client';
 
 import App from './app/app';
@@ -9,6 +9,8 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <StrictMode>
-    <App />
+    <Suspense fallback={<div>Loading...</div>}>
+      <App />
+    </Suspense>
   </StrictMode>
 );
