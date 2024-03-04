a2d064582ed13f306d7edfa5030ea5e9ddd7ee06
PerIngeVaaje <per.inge.vaaje@ssb.no>
2024-03-04 15:44:59 +0100

    PXWEB2-46 Refactor with icons

--- libs/pxweb2-ui/src/lib/components/Link/Link.tsx
+++ libs/pxweb2-ui/src/lib/components/Link/Link.tsx
@@ -1,17 +1,39 @@
 import React from 'react';
-import styles from './Link.module.scss';
+import classes from './Link.module.scss';
+import { Icon, IconProps } from '../Icon/Icon';
 
-interface LinkProps {
+interface LinkProps extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
+  icon?: IconProps['iconName'];
+  iconPosition?: 'left' | 'right';
   children: React.ReactNode;
   href: string;
 }
-
-export const Link: React.FC<LinkProps> = ({ children, href }) => {
+export function Link({
+  children,
+  href,
+  icon,
+  iconPosition,
+  ...rest
+}: LinkProps) {
   return (
-    <a href={href} className={styles.link}>
+    <a href={href} className={classes.link}>
+      {icon && iconPosition === 'left' && (
+        <Icon iconName={icon} className=""></Icon>
+      )}
       {children}
+      {icon && iconPosition === 'right' && (
+        <Icon iconName={icon} className=""></Icon>
+      )}
     </a>
   );
-};
+}
+
+// export const Link: React.FC<LinkProps> = ({ children, href }) => {
+//   return (
+//     <a href={href} className={styles.link}>
+//       {children}
+//     </a>
+//   );
+// };
 
 export default Link;
