a03d7b80e6fc3e6365fcb37c9cf3843a81c195d9
Kent Mossbäck <kent.mossback@scb.se>
2024-03-05 19:10:46 +0100

    Work on styling on search component

--- libs/pxweb2-ui/src/lib/components/Search/Search.tsx
+++ libs/pxweb2-ui/src/lib/components/Search/Search.tsx
@@ -3,6 +3,7 @@ import classes from './Search.module.scss';
 import { Icon, IconProps } from '../Icon/Icon';
 import{Label} from '../Typography/Label/Label';
 import React from 'react';
+import {Button, ButtonProps} from '../Button/Button';
 
 export interface SearchProps 
 extends React.InputHTMLAttributes <HTMLInputElement> {
@@ -10,27 +11,37 @@ extends React.InputHTMLAttributes <HTMLInputElement> {
   label?: boolean; 
   labelText?: string;
   searchPlaceHolder?: string;
-  icon?: IconProps['iconName'];  
+  searchIcon?: IconProps['iconName']; 
+  showClearButton?: boolean;
+  clearButton?: ButtonProps['icon'];  
 }
 
-
 export function Search({
-  icon = "MagnifyingGlass",
+  searchIcon = "MagnifyingGlass",
   label = false,
   variant,
   labelText,
-  searchPlaceHolder,
+  searchPlaceHolder,  
+  showClearButton = true,
+  clearButton = "XMark",
   ...rest
 }: SearchProps) { return (
   <div>
     {label && <Label>{labelText}</Label>}
     <div className={cl(classes.wrapper, classes.border, classes[variant])}>
-      <Icon iconName={icon}></Icon>
+      <Icon iconName={searchIcon} className={classes.searchIcon} ></Icon>
       <input type='text'
         className={cl(classes.input, classes[variant])}
         placeholder={searchPlaceHolder}
         {...rest}
       ></input>
+     {showClearButton && <Button 
+        variant={"primary"} 
+        icon={clearButton}  
+        //onClick={test}
+        size={'small'} 
+        className={classes.clearButton}>
+      </Button>}      
     </div>
   </div>
 );
