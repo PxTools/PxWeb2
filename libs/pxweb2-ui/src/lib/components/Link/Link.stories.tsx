cf2aeae702bc9798cf720e84118221c11a2a4912
PerIngeVaaje <per.inge.vaaje@ssb.no>
2024-03-07 14:48:26 +0100

    PXWEB2-46 Removed Typography. Should be inherited from parents. Added class for icons. Added properties for inline and noUnderline

--- libs/pxweb2-ui/src/lib/components/Link/Link.stories.tsx
+++ libs/pxweb2-ui/src/lib/components/Link/Link.stories.tsx
@@ -1,8 +1,6 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj, StoryFn } from '@storybook/react';
 import { Link } from './Link';
-
-/* import { within } from '@storybook/testing-library';
-import { expect } from '@storybook/jest'; */
+import { BodyLong } from '../Typography/BodyLong/BodyLong';
 
 const meta: Meta<typeof Link> = {
   component: Link,
@@ -12,10 +10,65 @@ export default meta;
 
 type Story = StoryObj<typeof Link>;
 
-export const Primary: Story = {
+export const variants: Story = {
   args: {
     href: '#',
-    children: 'En godt skrevet lenketekst',
-    icon: 'FileText',
+    children: 'En godt skrevet lenketekst'
   },
 };
+
+
+export const inlineAndStandalone: StoryFn<typeof BodyLong> = () => {
+  return (
+    <>
+      <h1>Inline link in BodyLong</h1>
+
+      <h2>medium without icon:</h2>      
+      <BodyLong>Gå til <Link href="#" inline >Statistikkbanken</Link> dersom du vil se flere tabeller </BodyLong>
+
+      <h2>small without icon right:</h2>
+      <BodyLong size='small'>Gå til <Link href="#" inline>Statistikkbanken</Link> dersom du vil se flere tabeller </BodyLong>
+
+      <h2>medium icon left:</h2>      
+      <BodyLong>Gå til <Link href="#" inline icon="FileText" iconPosition="left">Statistikkbanken</Link> dersom du vil se flere tabeller </BodyLong>
+
+      <h2>small icon left:</h2>
+      <BodyLong size='small'>Gå til <Link href="#" inline icon="FileText" iconPosition="left">Statistikkbanken</Link> dersom du vil se flere tabeller </BodyLong>
+
+
+      <h2>medium icon right:</h2>      
+      <BodyLong>Gå til <Link href="#" inline icon="FileText" iconPosition="right">Statistikkbanken</Link> dersom du vil se flere tabeller </BodyLong>
+
+      <h2>small icon right:</h2>
+      <BodyLong size='small'>Gå til <Link href="#"  inline icon="FileText" iconPosition="right">Statistikkbanken</Link> dersom du vil se flere tabeller </BodyLong>
+
+      
+      <h1>Standalone link in BodyLong</h1>
+
+      <h2>medium without icon:</h2>      
+      <BodyLong><Link href="#">Statistikkbanken</Link></BodyLong>
+
+      
+      <h2>small without icon:</h2>   
+      <BodyLong size='small'><Link href="#">Statistikkbanken</Link></BodyLong>
+
+      <h2>small icon left:</h2>
+      <BodyLong size='small'><Link href="#" icon="FileText" iconPosition="left">Statistikkbanken </Link></BodyLong>
+      
+      <h2>medium icon left:</h2>      
+      <BodyLong><Link href="#" icon="FileText" iconPosition="left">Statistikkbanken</Link></BodyLong>
+
+      <h2>medium icon right:</h2>      
+      <BodyLong><Link href="#" icon="FileText" iconPosition="right">Statistikkbanken</Link></BodyLong>
+
+      <h2>small icon right:</h2>
+      <BodyLong size='small'><Link href="#" icon="FileText" iconPosition="right">Statistikkbanken</Link></BodyLong>
+
+      <h2>Short text less then 24px:</h2>
+     <Link href="#" noUnderline>x</Link><br />
+    </>
+  );
+};
+
+/* import { within } from '@storybook/testing-library';
+import { expect } from '@storybook/jest'; */
--- libs/pxweb2-ui/src/lib/components/Link/Link.tsx
+++ libs/pxweb2-ui/src/lib/components/Link/Link.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import classes from './Link.module.scss';
+import cl from 'clsx';
 import { Icon, IconProps } from '../Icon/Icon';
 
 interface LinkProps extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
@@ -7,22 +8,28 @@ interface LinkProps extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
   iconPosition?: 'left' | 'right';
   children: React.ReactNode;
   href: string;
+  inline?:boolean;
+  noUnderline?:boolean
 }
 export function Link({
   children,
   href,
   icon,
   iconPosition,
+  inline=false,
+  noUnderline=false,
   ...rest
 }: LinkProps) {
   return (
-    <a href={href} className={classes.link}>
+    <a href={href} className={cl(classes.link, {
+      [classes.no_underline]: noUnderline,[classes.inline]: inline
+    })}   >
       {icon && iconPosition === 'left' && (
-        <Icon iconName={icon} className=""></Icon>
+        <Icon iconName={icon} className={cl({[classes.link_inline_svg]:inline})}></Icon>
       )}
       {children}
       {icon && iconPosition === 'right' && (
-        <Icon iconName={icon} className=""></Icon>
+        <Icon iconName={icon} className={cl({[classes.link_inline_svg]:inline})}></Icon>
       )}
     </a>
   );
