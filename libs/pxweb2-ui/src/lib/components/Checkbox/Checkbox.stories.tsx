db6d141646fb1fd30a1f8489c2fe64bfb076743d
Michael Pande <michaelpande@gmail.com>
2024-04-03 16:07:33 +0200

    Checkbox (#67)
    Checkbox, Mixed State Checkbox, Multiline Checkbox+++

---
+++ libs/pxweb2-ui/src/lib/components/Checkbox/Checkbox.stories.tsx
@@ -0,0 +1,160 @@
+import type { Meta, StoryFn } from '@storybook/react';
+import React from 'react';
+
+import { Checkbox, MixedCheckbox } from './Checkbox';
+
+const meta: Meta<typeof Checkbox> = {
+  component: Checkbox,
+  title: 'Components/Checkbox',
+};
+export default meta;
+
+export const MixedStateCheckbox: StoryFn<typeof Checkbox> = () => {
+  const [selectedMalamute, setSelectedMalamute] = React.useState(true);
+  const [selectedHusky, setSelectedHusky] = React.useState(true);
+  const [selectedYorkshireTerrier, setSelectedYorkshireTerrier] =
+    React.useState(false);
+  const [allSelected, setAllSelected] = React.useState<
+    'mixed' | 'true' | 'false'
+  >('mixed');
+
+  React.useEffect(() => {
+    if (!selectedHusky && !selectedMalamute && !selectedYorkshireTerrier) {
+      setAllSelected('false');
+    } else if (selectedHusky && selectedMalamute && selectedYorkshireTerrier) {
+      setAllSelected('true');
+    } else {
+      setAllSelected('mixed');
+    }
+  }, [selectedMalamute, selectedHusky, selectedYorkshireTerrier]);
+
+  return (
+    <>
+      <MixedCheckbox
+        id="test"
+        text="Select all"
+        onChange={(val) => {
+          if (selectedHusky && selectedMalamute && selectedYorkshireTerrier) {
+            setSelectedHusky(false);
+            setSelectedMalamute(false);
+            setSelectedYorkshireTerrier(false);
+          }
+          if (
+            !selectedHusky ||
+            !selectedMalamute ||
+            !selectedYorkshireTerrier
+          ) {
+            setSelectedHusky(true);
+            setSelectedMalamute(true);
+            setSelectedYorkshireTerrier(true);
+          }
+        }}
+        ariaControls={['var1', 'var2', 'var3']}
+        value={allSelected}
+        strong={true}
+      />
+
+      <Checkbox
+        id="var1"
+        text="Husky"
+        onChange={(val) => {
+          setSelectedHusky(val);
+        }}
+        value={selectedHusky}
+      />
+      <Checkbox
+        id="var2"
+        text="Malamute"
+        onChange={(val) => {
+          setSelectedMalamute(val);
+        }}
+        value={selectedMalamute}
+      />
+      <Checkbox
+        id="var3"
+        text="Yorkshire Terrier"
+        onChange={(val) => {
+          setSelectedYorkshireTerrier(val);
+        }}
+        value={selectedYorkshireTerrier}
+      />
+    </>
+  );
+};
+
+export const LongTextOn400pxWideMax: StoryFn<typeof Checkbox> = () => {
+  const [selectedVar1, setSelectedVar1] = React.useState(true);
+  const [selectedVar2, setSelectedVar2] = React.useState(true);
+  const [selectedVar3, setSelectedVar3] = React.useState(false);
+
+  return (
+    <div style={{ width: '400px' }}>
+      <p>
+        An example of how labeltext wraps on multiple lines in smaller spaces
+      </p>
+      <Checkbox
+        id="var1"
+        text="The text on this checkbox spans multiple lines. That's why it is written so long. Here's an additional sentence."
+        onChange={(val) => {
+          setSelectedVar2(val);
+        }}
+        value={selectedVar2}
+      />
+
+      <Checkbox
+        id="var2"
+        text="Short text"
+        onChange={(val) => {
+          setSelectedVar1(val);
+        }}
+        value={selectedVar1}
+      />
+      <Checkbox
+        id="var3"
+        text="The text on this checkbox spans multiple lines. That's why it is written so long. Here's an additional sentence."
+        onChange={(val) => {
+          setSelectedVar3(val);
+        }}
+        value={selectedVar3}
+      />
+    </div>
+  );
+};
+export const NoMargin: StoryFn<typeof Checkbox> = () => {
+  const [selectedVar1, setSelectedVar1] = React.useState(true);
+  const [selectedVar2, setSelectedVar2] = React.useState(true);
+  const [selectedVar3, setSelectedVar3] = React.useState(false);
+
+  return (
+    <div style={{ width: '400px' }}>
+      <Checkbox
+        id="var1"
+        text="No margin"
+        onChange={(val) => {
+          setSelectedVar2(val);
+        }}
+        value={selectedVar2}
+        noMargin={true}
+      />
+
+      <Checkbox
+        id="var2"
+        text="No margin"
+        onChange={(val) => {
+          setSelectedVar1(val);
+        }}
+        value={selectedVar1}
+        noMargin={true}
+      />
+      <Checkbox
+        id="var3"
+        text="Margin"
+        onChange={(val) => {
+          setSelectedVar3(val);
+        }}
+        value={selectedVar3}
+        noMargin={false}
+      />
+    </div>
+  );
+};
---
+++ libs/pxweb2-ui/src/lib/components/Checkbox/Checkbox.tsx
@@ -0,0 +1,129 @@
+import React from 'react';
+import cl from 'clsx';
+
+import styles from './Checkbox.module.scss';
+import { Icon } from '../Icon/Icon';
+import Label from '../Typography/Label/Label';
+
+interface CheckboxProps {
+  id: string;
+  text: string;
+  value: boolean;
+  onChange: (str: boolean) => void;
+  tabIndex?: number;
+  strong?: boolean;
+  noMargin?: boolean;
+}
+
+export const Checkbox: React.FC<CheckboxProps> = ({
+  id,
+  value,
+  text,
+  onChange,
+  tabIndex,
+  strong,
+  noMargin,
+}) => {
+  return (
+    <div
+      id={id}
+      role="checkbox"
+      aria-checked={value}
+      aria-labelledby={id + '-label'}
+      className={styles.checkboxWrapper}
+      tabIndex={tabIndex ? tabIndex : 0}
+      onKeyUp={(event) => {
+        if (event.key === ' ' || event.key === 'Enter') {
+          event.preventDefault();
+          onChange(!value);
+        }
+      }}
+      onClick={(event) => {
+        event.preventDefault();
+        onChange(!value);
+      }}
+    >
+      <span
+        className={cl(styles.checkmark, {
+          [styles.checked]: value,
+          [styles.checkmarkWithoutMargin]: noMargin,
+        })}
+      >
+        {value && <Icon iconName="CheckMark"></Icon>}
+      </span>
+      <div className={styles.label} id={id + '-label'}>
+        <span className={cl({ [styles.strong]: strong })}>{text}</span>
+      </div>
+    </div>
+  );
+};
+interface MixedCheckboxProps {
+  id: string;
+  text: string;
+  value: 'mixed' | 'false' | 'true';
+  onChange: (str: string) => void;
+  ariaControls: string[];
+  tabIndex?: number;
+  strong?: boolean;
+  noMargin?: boolean;
+}
+export const MixedCheckbox: React.FC<MixedCheckboxProps> = ({
+  id,
+  value,
+  text,
+  onChange,
+  ariaControls,
+  tabIndex,
+  strong,
+  noMargin,
+}) => {
+  return (
+    <div
+      id={id}
+      role="checkbox"
+      aria-checked={value}
+      aria-labelledby={id + '-label'}
+      aria-controls={ariaControls.join(' ')}
+      className={styles.checkboxWrapper}
+      tabIndex={tabIndex ? tabIndex : 0}
+      onKeyUp={(event) => {
+        if (event.key === ' ' || event.key === 'Enter') {
+          event.preventDefault();
+          if (value === 'false') {
+            onChange('true');
+          }
+          if (value === 'mixed' || value === 'true') {
+            onChange('false');
+          }
+        }
+      }}
+      onClick={(event) => {
+        event.preventDefault();
+        if (value === 'false') {
+          onChange('true');
+        }
+        if (value === 'mixed' || value === 'true') {
+          onChange('false');
+        }
+      }}
+    >
+      <span
+        className={cl(styles.checkmark, {
+          [styles.checked]: value === 'mixed' || value === 'true',
+          [styles.checkmarkWithoutMargin]: noMargin,
+        })}
+      >
+        {value === 'true' && <Icon iconName="CheckMark"></Icon>}
+        {value === 'mixed' && <Icon iconName="IndeterminateCheckMark"></Icon>}
+      </span>
+      <div
+        className={cl(styles.label, { [styles.strong]: strong })}
+        id={id + '-label'}
+      >
+        {text}
+      </div>
+    </div>
+  );
+};
+
+export default Checkbox;
