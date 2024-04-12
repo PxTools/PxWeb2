7595ecf0be16b5d3fc8bbc98f665d8f423d6d271
MikaelNordberg <mikael.nordberg@scb.se>
2024-04-12 10:41:29 +0200

    Updated tests and stories with onChange event

--- libs/pxweb2-ui/src/lib/components/Select/Select.spec.tsx
+++ libs/pxweb2-ui/src/lib/components/Select/Select.spec.tsx
@@ -4,9 +4,13 @@ import Select, { SelectOption } from './Select';
 
 const options: SelectOption[] = [{ label: 'Option 1', value: '1' }, { label: 'Option 2', value: '2'}, { label: 'Option 3', value: '3'}];
 
+function selectedOptionChanged(selectedItem: SelectOption) {  
+  console.log('Selected option: ' + selectedItem.label);
+}
+
 describe('Select', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Select label="Label" options={options} />);
+    const { baseElement } = render(<Select label="Label" options={options} onChange={selectedOptionChanged}/>);
     expect(baseElement).toBeTruthy();
   });
 });
--- libs/pxweb2-ui/src/lib/components/Select/Select.stories.tsx
+++ libs/pxweb2-ui/src/lib/components/Select/Select.stories.tsx
@@ -10,12 +10,18 @@ export default meta;
 const options: SelectOption[] = [{ label: 'Option 1', value: '1' }, { label: 'Option 2', value: '2'}, { label: 'Option 3', value: '3'}];
 const defaultOption = 'Make selection';
 
+function selectedOptionChanged(selectedItem: SelectOption) {  
+  console.log('Selected option: ' + selectedItem.label);
+}
+
 export const Default = {
   args: {
     variant: 'default',
     label: 'Label',
     hideLabel: false,
+    options: options,
     defaultOption: defaultOption,
+    onChange: selectedOptionChanged,
   },
 };
 
@@ -25,10 +31,10 @@ export const Variant: StoryFn<typeof Select> = () => {
       <h1>Variant</h1>
 
       <h2>Default:</h2>
-      <Select label="Label" options={options} defaultOption={defaultOption}></Select>
+      <Select label="Label" options={options} defaultOption={defaultOption} onChange={selectedOptionChanged}></Select>
 
       <h2>inVariableBox:</h2>
-      <Select label="Label" variant='inVariableBox' options={options} defaultOption={defaultOption}></Select>
+      <Select label="Label" variant='inVariableBox' options={options} defaultOption={defaultOption} onChange={selectedOptionChanged}></Select>
     </>
   );
 };
@@ -39,10 +45,10 @@ export const ShowLabel: StoryFn<typeof Select> = () => {
       <h1>Hide Label</h1>
 
       <h2>With label:</h2>
-      <Select label="Label" options={options} defaultOption={defaultOption}></Select>
+      <Select label="Label" options={options} defaultOption={defaultOption} onChange={selectedOptionChanged}></Select>
 
       <h2>Without label:</h2>
-      <Select label="Label" hideLabel options={options} defaultOption={defaultOption}></Select>
+      <Select label="Label" hideLabel options={options} defaultOption={defaultOption} onChange={selectedOptionChanged}></Select>
     </>
   );
 };
@@ -53,10 +59,10 @@ export const SelectedOption: StoryFn<typeof Select> = () => {
       <h1>Selected option</h1>
 
       <h2>Selected option = 1:</h2>
-      <Select label="Label" options={options} defaultOption={defaultOption} selectedOption='1'></Select>
+      <Select label="Label" options={options} defaultOption={defaultOption} selectedOption='1' onChange={selectedOptionChanged}></Select>
 
       <h2>No selected option:</h2>
-      <Select label="Label" hideLabel options={options} defaultOption={defaultOption}></Select>
+      <Select label="Label" hideLabel options={options} defaultOption={defaultOption} onChange={selectedOptionChanged}></Select>
     </>
   );
 };
