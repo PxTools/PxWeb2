de10049f9c6d7c2b8f33ab781103de7a887b433d
Michael Pande <git@e2e.no>
2024-04-17 15:14:27 +0200

    Fixed broken import

--- apps/pxweb2/src/app/util/config/getConfig.ts
+++ apps/pxweb2/src/app/util/config/getConfig.ts
@@ -1,4 +1,4 @@
-import { Config } from '../../../../public/config/configType';
+import { Config } from './configType';
 export const getConfig = () => {
   return (window as any).PxWeb2Config as Config;
 };
