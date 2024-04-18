1f0cf3bdcd12f9d721dc42ff37d927709c0d7231
Kent Mossbäck <kent.mossback@scb.se>
2024-04-18 13:06:18 +0200

    Adjustements made efter code review

--- libs/pxweb2-ui/src/lib/components/Search/Search.stories.tsx
+++ libs/pxweb2-ui/src/lib/components/Search/Search.stories.tsx
@@ -1,7 +1,6 @@
 import type { Meta, StoryObj, StoryFn } from '@storybook/react';
 import { Search } from './Search';
 
-
 const meta: Meta<typeof Search> = {
   component: Search,
   title: 'Components/Search',
@@ -21,58 +20,55 @@ export const Default: Story = {
 };
 
 export const Variants: StoryFn<typeof Search> = () => {
-    return (
-      <>
-        Default
-        <br />
-        <br />
-        <Search
-          variant="default"
-          searchPlaceHolder={placeholder}          
-        >
-        </Search>
-        <br />
-        In a variable box
-        <br />
-        <br />
-        <Search
-          variant="inVariableBox"
-          searchPlaceHolder={placeholder}          
-        >
-        </Search>
-        <br />
-        Default with label 
-        <br />
-        <br />
-        <Search
-          showLable={true}
-          variant="default"
-          searchPlaceHolder={placeholder}          
-          labelText= {labelText}          
-        >
-        </Search>
-        <br />        
-        Default without label 
-        <br />
-        <br />
-        <Search
-          variant="default"
-          searchPlaceHolder={placeholder}          
-        >
-        </Search>
-        <br />        
-        In variblebox  
-        <br />
-        <br />
-        <Search
-          variant="inVariableBox"
-          searchPlaceHolder={placeholder}          
-        >
-        </Search>
-        <br />       
-        </>
+  return (
+    <>
+      Default
+      <br />
+      <br />
+      <Search
+        variant="default"
+        searchPlaceHolder={placeholder}          
+      >
+      </Search>
+      <br />
+      In a variable box
+      <br />
+      <br />
+      <Search
+        variant="inVariableBox"
+        searchPlaceHolder={placeholder}          
+      >
+      </Search>
+      <br />
+      Default with label 
+      <br />
+      <br />
+      <Search
+        showLable={true}
+        variant="default"
+        searchPlaceHolder={placeholder}          
+        labelText={labelText}          
+      >
+      </Search>
+      <br />        
+      Default without label 
+      <br />
+      <br />
+      <Search
+        variant="default"
+        searchPlaceHolder={placeholder}          
+      >
+      </Search>
+      <br />        
+      In variable box  
+      <br />
+      <br />
+      <Search
+        variant="inVariableBox"
+        searchPlaceHolder={placeholder}          
+      >
+      </Search>
+      <br />       
+    </>
   );
- 
-};    
-
-
+}; 
--- libs/pxweb2-ui/src/lib/components/Search/Search.tsx
+++ libs/pxweb2-ui/src/lib/components/Search/Search.tsx
@@ -1,15 +1,16 @@
-import cl from 'clsx';
-import classes from './Search.module.scss';
-import {Icon} from '../Icon/Icon';
-import {Label} from '../Typography/Label/Label';
-import {Button} from '../Button/Button';
 import React, { useState, useRef } from 'react';
+import cl from 'clsx';
 
-export interface SearchProps 
-extends React.InputHTMLAttributes <HTMLInputElement> {
-  variant: 'default' | 'inVariableBox' ;
+import classes from './Search.module.scss';
+import { Icon } from '../Icon/Icon';
+import { Label } from '../Typography/Label/Label';
+import { Button } from '../Button/Button';
+
+
+export interface SearchProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  variant: 'default' | 'inVariableBox';
   labelText?: string;
-  searchPlaceHolder?: string;  
+  searchPlaceHolder?: string;
   showLable?: boolean;
   ariaLabelIconText?: string;
   arilLabelButtonText?: string;
@@ -18,53 +19,54 @@ extends React.InputHTMLAttributes <HTMLInputElement> {
 export function Search({
   variant,
   labelText,
-  searchPlaceHolder,  
-  showLable = false,  
-  ariaLabelIconText =  'Search icon',
+  searchPlaceHolder,
+  showLable = false,
+  ariaLabelIconText = 'Search icon',
   arilLabelButtonText = 'Clear search button',
   ...rest
-}: SearchProps) { 
+}: SearchProps) {
   const [inputValue, setInputValue] = useState('');
-  const inputRef = useRef<HTMLInputElement>(null); 
+  const inputRef = useRef<HTMLInputElement>(null);
+
   const handleClear = () => {
-      setInputValue('');
-      if (inputRef.current !== null) {
-        inputRef.current.focus();
-      }
+    setInputValue('');
+    if (inputRef.current !== null) {
+      inputRef.current.focus();
+    }
   };
-  const handleKeyDown = (e: { keyCode: number; }) => {
+
+  const handleKeyDown = (e: { keyCode: number }) => {
     if (e.keyCode === 27) {
       handleClear();
     }
   };
-   const hasValue = inputValue.length > 0;
+
+  const hasValue = inputValue.length > 0;
+
   return (
     <div className={classes.search}>
-      {showLable && <Label size='medium' >{labelText}</Label>}
+      {showLable && <Label size='medium'>{labelText}</Label>}
       <div className={cl(classes.wrapper, classes.border, classes[variant])}>
-        <Icon 
-          iconName="MagnifyingGlass" 
-          className={classes.searchIcon} 
-          aria-label={ariaLabelIconText}>
-        </Icon>
-        <input type='text'
+        <Icon iconName='MagnifyingGlass' className={classes.searchIcon} aria-label={ariaLabelIconText}></Icon>
+        <input
+          type='text'
           ref={inputRef}
           className={cl(classes.input, classes[variant])}
-          placeholder={searchPlaceHolder}           
+          placeholder={searchPlaceHolder}
           value={inputValue}
           onChange={(e) => setInputValue(e.target.value)}
           onKeyDown={handleKeyDown}
           {...rest}
         ></input>
         {hasValue && (
-          <Button 
-            variant="tertiary"
-            icon="XMark"  
-            size="small"
+          <Button
+            variant='tertiary'
+            icon='XMark'
+            size='small'
             onClick={handleClear}
             aria-label={arilLabelButtonText}
           ></Button>
-        )}      
+        )}
       </div>
     </div>
   );
