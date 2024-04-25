9d1f2a92e0e5a757fa31545a826aa25c3ab9ba89
MikaelNordberg <mikael.nordberg@scb.se>
2024-04-25 12:44:28 +0200

    Made tests work

--- libs/pxweb2-ui/src/lib/components/Select/Select.spec.tsx
+++ libs/pxweb2-ui/src/lib/components/Select/Select.spec.tsx
@@ -4,8 +4,8 @@ import Select, { SelectOption } from './Select';
 
 const options: SelectOption[] = [{ label: 'Option 1', value: '1' }, { label: 'Option 2', value: '2'}, { label: 'Option 3', value: '3'}];
 
-function selectedOptionChanged(selectedItem: SelectOption) {  
-  console.log('Selected option: ' + selectedItem.label);
+function selectedOptionChanged(selectedItem: SelectOption | undefined) {  
+  selectedItem ? console.log('Selected option: ' + selectedItem.label) : console.log('No option selected');
 }
 
 describe('Select', () => {
