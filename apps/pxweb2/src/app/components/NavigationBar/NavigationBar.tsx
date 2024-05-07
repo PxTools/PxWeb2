6e24846571b15947501cba2b2d565d5b162e8535
Michael Pande <git@e2e.no>
2024-05-07 14:48:06 +0200

    Combine multiple elements, languages and sidebar

--- apps/pxweb2/src/app/components/NavigationBar/NavigationBar.tsx
+++ apps/pxweb2/src/app/components/NavigationBar/NavigationBar.tsx
@@ -2,35 +2,59 @@ import React from 'react';
 import styles from './NavigationBar.module.scss';
 import { useTranslation } from 'react-i18next';
 import { Item } from '../NavigationRail/NavigationRail';
+import { NavigationItem } from '../../app';
 
-export const NavigationBar: React.FC = () => {
+interface NavigationBarProps {
+  selected: NavigationItem;
+  onChange: (newSelected: NavigationItem) => void;
+}
+export const NavigationBar: React.FC<NavigationBarProps> = ({
+  selected,
+  onChange,
+}) => {
   const { t } = useTranslation();
+
   return (
     <div className={styles.navigationBar}>
       <Item
         label={t('presentation_page.sidemenu.selection.title')}
-        selected={true}
+        selected={selected === 'filter'}
         icon={'Controls'}
+        onClick={() => {
+          onChange('filter');
+        }}
       />
       <Item
         label={t('presentation_page.sidemenu.view.title')}
-        selected={false}
+        selected={selected === 'view'}
         icon={'BarChart'}
+        onClick={() => {
+          onChange('view');
+        }}
       />
       <Item
         label={t('presentation_page.sidemenu.edit.title')}
-        selected={false}
+        selected={selected === 'edit'}
         icon={'ArrowsUpDown'}
+        onClick={() => {
+          onChange('edit');
+        }}
       />
       <Item
         label={t('presentation_page.sidemenu.save.title')}
-        selected={false}
+        selected={selected === 'save'}
         icon={'FloppyDisk'}
+        onClick={() => {
+          onChange('save');
+        }}
       />
       <Item
         label={t('presentation_page.sidemenu.help.title')}
-        selected={false}
+        selected={selected === 'help'}
         icon={'QuestionMarkCircle'}
+        onClick={() => {
+          onChange('help');
+        }}
       />
     </div>
   );
