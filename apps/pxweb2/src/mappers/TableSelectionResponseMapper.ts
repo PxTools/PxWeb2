80fd6230a46b223ab63c353a00de4e9a05d8c804
Sjur Sutterud Sagen <sjs@ssb.no>
2024-06-17 10:03:20 +0200

    Add default selection to the VariableBox (#187)
    Since we need to show the selections made in the default selection
    algorithm, we need to add this to the logic for the table view/app.tsx.
    This is a first version to get it working, with a basic test for the
    mapper created to map the response from the API.
    
    I did find that the current url created is too long in the large table
    we test on, therefore I have only tested this one the three smaller test
    tables in the app.

---
+++ apps/pxweb2/src/mappers/TableSelectionResponseMapper.ts
@@ -0,0 +1,22 @@
+import { SelectionResponse } from '@pxweb2/pxweb2-api-client';
+import { SelectedVBValues } from '@pxweb2/pxweb2-ui';
+
+export function mapTableSelectionResponse(
+  response: SelectionResponse
+): SelectedVBValues[] {
+  // TODO:  Check if selection.selection is intended,
+  const selectedVBValues: SelectedVBValues[] = response.selection.selection.map(
+    (variable) => {
+      return {
+        id: variable.variableCode,
+
+        // TODO: Not ready in the API yet, implement when ready
+        //       handleCodeListChange in app.tsx probably needs to be refactored for this to work optimally
+        selectedCodeList: undefined,
+        values: variable.valueCodes ? variable.valueCodes : [],
+      };
+    }
+  );
+
+  return selectedVBValues;
+}
