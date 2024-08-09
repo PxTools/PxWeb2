ccc6b0462bccd8dba0a1c129ed880e8d31e86d02
PerIngeVaaje <per.inge.vaaje@ssb.no>
2024-08-09 15:36:34 +0200

    Import StoryFn

--- libs/pxweb2-ui/src/lib/components/Spinner/Spinner.stories.tsx
+++ libs/pxweb2-ui/src/lib/components/Spinner/Spinner.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj,StoryFn } from '@storybook/react';
 import { Spinner } from './Spinner';
 
 const meta: Meta<typeof Spinner> = {
