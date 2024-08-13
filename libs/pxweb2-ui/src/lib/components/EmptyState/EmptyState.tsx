5a6ba2890af2b7f679cea0dbf648210c111cf193
Sjur Sutterud Sagen <sjs@ssb.no>
2024-08-13 14:25:28 +0200

    Refactor illustrations
    Refactoring the illustrations to contain more of the information for
    each version, after discussions with other devs and the designer.
    
    Now the versions contain the viewBox sizes, and also have colors
    imported from the CSS of the theme, instead of being hardcoded. We will
    evaluate at a later time, if we should rename those colors or not, since
    we want users to be able to change the colors in the illustrations.

--- libs/pxweb2-ui/src/lib/components/EmptyState/EmptyState.tsx
+++ libs/pxweb2-ui/src/lib/components/EmptyState/EmptyState.tsx
@@ -19,7 +19,6 @@ export function EmptyState({
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
   let illustration = Illustrations[svgName].large;
-  let viewBoxSizes = '0 0 277 144';
   const breakpoint = Number(BreakpointsXsmallMaxWidth.replace('px', ''));
 
   useEffect(() => {
@@ -36,34 +35,11 @@ export function EmptyState({
 
   if (breakpoint > windowWidth) {
     illustration = Illustrations[svgName].small;
-    viewBoxSizes = '0 0 208 108';
   }
 
   if (!illustration) {
     return null; // TODO:Add error message here?
   }
-
-  /*
-   * TODO:
-   * - Ask about viewBox values and figma, one illustrantion has them, the other has many groups instead
-   * - - viewBox="0 0 277 144" viewBox="0 0 208 108" are rounded up from figma
-   * - - should these be hardcoded like this? will all illustrations have the same viewBox values? for large and small?
-   * - how do we handle the colors of the illustrations? should you be able to customize them?
-   * - - the colors are now hardcoded, since one illustration has many lines of different colors
-   * - - - can fix, but wanted?
-   * - - in figma some colors are name as "bluegray" etc,
-   *     but should we use color names here? then if someone wants to change the colors to non-blue
-   *     they will have to overwrite "bluegray" with green for example
-   *        maybe better to never use such color names?
-   * - Fix the styling
-   * - - What in EmptyState in figma is there only to make it look good in figma? What should be ignored there?
-   * - - The breakpoint seems to not look correct all the time, 500ish width starts clipping the illustration
-   * - - - see below
-   *
-   * -- Hvor jeg var --
-   *  - Jeg driver å ser på CSS og breakpoints. Det er problemer med selve Content-Containeren. Den har ikke riktig margin og padding.
-   *    Hvis jeg forandrer på det, så ser selve EmptyState riktig ut. Ihvertfall på den minste størrelsen.
-   */
 
   return (
     <div className={cl(styles['empty-state'])}>
@@ -71,11 +47,11 @@ export function EmptyState({
         <div className={cl(styles['empty-state-illustration'])}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            viewBox={viewBoxSizes}
+            viewBox={illustration.viewBox}
             role="img"
             aria-label={headingTxt + '. ' + descriptionTxt}
           >
-            {illustration}
+            {illustration.paths}
           </svg>
         </div>
         <div className={cl(styles['empty-state-text'])}>
