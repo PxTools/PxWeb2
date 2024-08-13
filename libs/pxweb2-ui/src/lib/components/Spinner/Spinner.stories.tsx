26da0a96f0c59d1e38d49ee0a368d75300ee618a
PerIngeVaaje <per.inge.vaaje@ssb.no>
2024-08-13 14:13:57 +0200

    PXWEB2-216 Format document

--- libs/pxweb2-ui/src/lib/components/Spinner/Spinner.stories.tsx
+++ libs/pxweb2-ui/src/lib/components/Spinner/Spinner.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj,StoryFn } from '@storybook/react';
+import type { Meta, StoryFn } from '@storybook/react';
 import { Spinner } from './Spinner';
 
 const meta: Meta<typeof Spinner> = {
@@ -6,7 +6,6 @@ const meta: Meta<typeof Spinner> = {
   title: 'Components/Spinner',
 };
 export default meta;
-type Story = StoryObj<typeof Spinner>;
 
 export const Options = {
   args: {
@@ -15,7 +14,7 @@ export const Options = {
 
   argTypes: {
     size: {
-      options: ['xsmall', 'small','medium','large','xlarge'],
+      options: ['xsmall', 'small', 'medium', 'large', 'xlarge'],
       control: { type: 'radio' },
     },
   },
@@ -24,49 +23,57 @@ export const Options = {
 export const size: StoryFn<typeof Spinner> = () => {
   return (
     <>
-    <div>xsmall
-    <Spinner size='xsmall'></Spinner>
-    </div>
-    <div>small
-    <Spinner size='small'></Spinner>
-    </div>
-    <div>medium
-    <Spinner size='medium'></Spinner>
-    </div>
-    <div>large
-    <Spinner size='large'></Spinner>
-    </div>
-    <div>xlarge
-    <Spinner size='xlarge'></Spinner>
-    </div>
+      <div>
+        xsmall
+        <Spinner size="xsmall"></Spinner>
+      </div>
+      <div>
+        small
+        <Spinner size="small"></Spinner>
+      </div>
+      <div>
+        medium
+        <Spinner size="medium"></Spinner>
+      </div>
+      <div>
+        large
+        <Spinner size="large"></Spinner>
+      </div>
+      <div>
+        xlarge
+        <Spinner size="xlarge"></Spinner>
+      </div>
     </>
   );
-}
+};
 
 export const variants: StoryFn<typeof Spinner> = () => {
   return (
     <>
-    <div>default
-    <Spinner></Spinner>
-    </div>
-    <div>inverted
-    <Spinner variant='inverted'></Spinner>
-    </div>
+      <div>
+        default
+        <Spinner></Spinner>
+      </div>
+      <div>
+        inverted
+        <Spinner variant="inverted"></Spinner>
+      </div>
     </>
   );
-}
+};
 export const label: StoryFn<typeof Spinner> = () => {
   return (
     <>
-    <div>default
-    <Spinner label='Tabellen lastes...'></Spinner>
-    </div>
-    <div>inverted
-      <div style={{backgroundColor:"red",width:"300px"}}>
-    <Spinner variant='inverted' label='Tabellen lastes...'></Spinner>
-    </div>
-    </div>
+      <div>
+        default
+        <Spinner label="Tabellen lastes..."></Spinner>
+      </div>
+      <div>
+        inverted
+        <div style={{ backgroundColor: 'red', width: '300px' }}>
+          <Spinner variant="inverted" label="Tabellen lastes..."></Spinner>
+        </div>
+      </div>
     </>
   );
-}
-
+};
--- libs/pxweb2-ui/src/lib/components/Spinner/Spinner.tsx
+++ libs/pxweb2-ui/src/lib/components/Spinner/Spinner.tsx
@@ -5,41 +5,31 @@ export interface SpinnerProps {
   size?: 'xsmall' | 'small' | 'medium' | 'large' | 'xlarge';
   variant?: 'default' | 'inverted';
   label?: string;
-  children?: string | React.ReactNode;
 }
 
 export function Spinner({
   size = 'medium',
   variant = 'default',
   label,
-  children,
 }: SpinnerProps) {
-  let PathStrokeSubtle= "";
-  let PathStrokeAction= "";
+  let PathStrokeSubtle = '';
+  let PathStrokeAction = '';
   let Eclipse = null;
   let Labelsize: 'small' | 'medium' = 'medium';
-  let LabelExist = false;
-  if (label!==undefined)
-    {
-      if (label.length > 0)
-        {
-          LabelExist =true;
-        }
-    }
 
   switch (variant) {
     case 'default':
-       PathStrokeSubtle= "var(--px-color-border-subtle)";
-       PathStrokeAction= "var(--px-color-border-action)";
+      PathStrokeSubtle = 'var(--px-color-border-subtle)';
+      PathStrokeAction = 'var(--px-color-border-action)';
       break;
-
-      case 'inverted':
-        PathStrokeSubtle= "var(--px-color-border-subtle)";
-       PathStrokeAction= "var(--px-color-border-on-inverted)";
+    case 'inverted':
+      PathStrokeSubtle = 'var(--px-color-border-subtle)';
+      PathStrokeAction = 'var(--px-color-border-on-inverted)';
       break;
-      default:
-        PathStrokeSubtle= "var(--px-color-border-subtle)";
-        PathStrokeAction= "var(--px-color-border-action)";
+    default:
+      PathStrokeSubtle = 'var(--px-color-border-subtle)';
+      PathStrokeAction = 'var(--px-color-border-action)';
+      break;
   }
   switch (size) {
     case 'xlarge':
@@ -50,29 +40,26 @@ export function Spinner({
           width="88px"
           height="88px"
           fill="none"
-          className={cl(classes[`spinner`], classes[`spinner-${size}`],classes[`loading`])}
+          className={cl(
+            classes[`spinner`],
+            classes[`spinner-${size}`],
+            classes[`loading`]
+          )}
         >
-            {/* <path
-           d="M4 44C4 33.3913 8.21428 23.2172 15.7157 15.7157C23.2172 8.21427 33.3913 4 44 4C54.6087 4 64.7828 8.21428 72.2843 15.7157C79.7857 23.2172 84 33.3913 84 44C84 54.6087 79.7857 64.7828 72.2843 72.2843C64.7828 79.7857 54.6087 84 44 84C33.3913 84 23.2172 79.7857 15.7157 72.2843C8.21427 64.7828 4 54.6087 4 44L4 44Z"           
+          <circle
+            cx="25"
+            cy="25"
+            r="22.5"
             className={cl(classes[`path-stroke-background-${variant}`])}
-            // stroke={PathStrokeSubtle}
-            stroke-width="8px"/> 
-          <path
-            d="M6.04602 31.3709C8.54382 23.8643 13.2115 17.2669 19.4588 12.4132C25.7061 7.55936 33.2524 4.6671 41.1434 4.10213C49.0345 3.53715 56.9159 5.32484 63.7909 9.23911C70.666 13.1534 76.2259 19.0184 79.7677 26.0926C83.3094 33.1668 84.6739 41.1324 83.6885 48.982C82.7032 56.8317 79.4123 64.2128 74.232 70.1921C69.0516 76.1715 62.2146 80.4804 54.5854 82.574C46.9562 84.6675 38.8775 84.4518 31.3709 81.954"
-            className={cl(classes[`path-stroke-circle-${variant}`])}
-            // stroke={PathStrokeSubtle}
-            stroke-width="8px"/> */}
-
-          <circle cx='25' cy='25' r='22.5'
-            className={cl(classes[`path-stroke-background-${variant}`])}
-            // stroke={PathStrokeSubtle}
-            stroke-width="4.54"></circle>
-         <circle cx='25' cy='25' r='22.5'
-            className={cl(classes[`path-stroke-circle-${variant}`])}
-            // stroke={PathStrokeAction}
             stroke-width="4.54"
-            ></circle>
-        
+          ></circle>
+          <circle
+            cx="25"
+            cy="25"
+            r="22.5"
+            className={cl(classes[`path-stroke-circle-${variant}`])}
+            stroke-width="4.54"
+          ></circle>
         </svg>
       );
       Labelsize = 'medium';
@@ -85,17 +72,26 @@ export function Spinner({
           width="64px"
           height="64px"
           fill="none"
-          className={cl(classes[`spinner`], classes[`spinner-${size}`],classes[`loading`])}
+          className={cl(
+            classes[`spinner`],
+            classes[`spinner-${size}`],
+            classes[`loading`]
+          )}
         >
-            <circle cx='25' cy='25' r='22.5'
+          <circle
+            cx="25"
+            cy="25"
+            r="22.5"
             className={cl(classes[`path-stroke-background-${variant}`])}
-            stroke-width="5.47">
-
-          </circle>
-          <circle cx='25' cy='25' r='22.5'
+            stroke-width="5.47"
+          ></circle>
+          <circle
+            cx="25"
+            cy="25"
+            r="22.5"
             className={cl(classes[`path-stroke-circle-${variant}`])}
-            stroke-width="5.47">
-        </circle>
+            stroke-width="5.47"
+          ></circle>
         </svg>
       );
       Labelsize = 'medium';
@@ -108,17 +104,26 @@ export function Spinner({
           width="44px"
           height="44px"
           fill="none"
-          className={cl(classes[`spinner`], classes[`spinner-${size}`],classes[`loading`])}
+          className={cl(
+            classes[`spinner`],
+            classes[`spinner-${size}`],
+            classes[`loading`]
+          )}
         >
-            <circle cx='25' cy='25' r='21.6'
+          <circle
+            cx="25"
+            cy="25"
+            r="21.6"
             className={cl(classes[`path-stroke-background-${variant}`])}
-            stroke-width="5.68">
-
-          </circle>
-          <circle cx='25' cy='25' r='21.6'
+            stroke-width="5.68"
+          ></circle>
+          <circle
+            cx="25"
+            cy="25"
+            r="21.6"
             className={cl(classes[`path-stroke-circle-${variant}`])}
-            stroke-width="5.68">
-        </circle>
+            stroke-width="5.68"
+          ></circle>
         </svg>
       );
       Labelsize = 'medium';
@@ -131,17 +136,26 @@ export function Spinner({
           width="32px"
           height="32px"
           fill="none"
-          className={cl(classes[`spinner`], classes[`spinner-${size}`],classes[`loading`])}
+          className={cl(
+            classes[`spinner`],
+            classes[`spinner-${size}`],
+            classes[`loading`]
+          )}
         >
-            <circle cx='25' cy='25' r='21.5'
+          <circle
+            cx="25"
+            cy="25"
+            r="21.5"
             className={cl(classes[`path-stroke-background-${variant}`])}
-            stroke-width="6.25">
-
-          </circle>
-          <circle cx='25' cy='25' r='21.5'
+            stroke-width="6.25"
+          ></circle>
+          <circle
+            cx="25"
+            cy="25"
+            r="21.5"
             className={cl(classes[`path-stroke-circle-${variant}`])}
-            stroke-width="6.25">
-        </circle>
+            stroke-width="6.25"
+          ></circle>
         </svg>
       );
       Labelsize = 'medium';
@@ -154,17 +168,26 @@ export function Spinner({
           width="20px"
           height="20px"
           fill="none"
-          className={cl(classes[`spinner`], classes[`spinner-${size}`],classes[`loading`])}
+          className={cl(
+            classes[`spinner`],
+            classes[`spinner-${size}`],
+            classes[`loading`]
+          )}
         >
-            <circle cx='25' cy='25' r='21.05'
+          <circle
+            cx="25"
+            cy="25"
+            r="21.05"
             className={cl(classes[`path-stroke-background-${variant}`])}
-            stroke-width="7.5">
-
-          </circle>
-          <circle cx='25' cy='25' r='21.05'
+            stroke-width="7.5"
+          ></circle>
+          <circle
+            cx="25"
+            cy="25"
+            r="21.05"
             className={cl(classes[`path-stroke-circle-${variant}`])}
-            stroke-width="7.5">
-        </circle>
+            stroke-width="7.5"
+          ></circle>
         </svg>
       );
       Labelsize = 'small';
@@ -174,33 +197,47 @@ export function Spinner({
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 50 50"
-          width="54px"
-          height="54px"
+          width="44px"
+          height="44px"
           fill="none"
-          className={cl(classes[`spinner`], classes[`spinner-${size}`],classes[`loading`])}
+          className={cl(
+            classes[`spinner`],
+            classes[`spinner-${size}`],
+            classes[`loading`]
+          )}
         >
-            <circle cx='25' cy='25' r='20'
+          <circle
+            cx="25"
+            cy="25"
+            r="21.6"
             className={cl(classes[`path-stroke-background-${variant}`])}
-            stroke-width="4.6">
-
-          </circle>
-          <circle cx='25' cy='25' r='20'
+            stroke-width="5.68"
+          ></circle>
+          <circle
+            cx="25"
+            cy="25"
+            r="21.6"
             className={cl(classes[`path-stroke-circle-${variant}`])}
-            stroke-width="4.6">
-        </circle>
+            stroke-width="5.68"
+          ></circle>
         </svg>
       );
       Labelsize = 'medium';
   }
 
   return (
-
     <div className={cl(classes[`spinner`], classes[`spinner-${size}`])}>
-      {/* <div className={cl(classes[`eclipse-${size}`],classes[`loading`])}>{Eclipse}</div> */}
       <div className={cl(classes[`eclipse-${size}`])}>{Eclipse}</div>
-      <label className={cl(classes[`label-${Labelsize}`],{[classes[`text-color-default`]]:variant==='default'},{[classes[`text-color-inverted`]]:variant==='inverted'})}>{label} </label>
+      <label
+        className={cl(
+          classes[`label-${Labelsize}`],
+          { [classes[`text-color-default`]]: variant === 'default' },
+          { [classes[`text-color-inverted`]]: variant === 'inverted' }
+        )}
+      >
+        {label}{' '}
+      </label>
     </div>
-
   );
 }
 
