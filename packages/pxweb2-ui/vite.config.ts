ae41221ee3652c1cc28970c7f8c658327efdef89
Michael Pande <git@e2e.no>
2024-11-27 18:18:49 +0100

    Fixed tests

--- packages/pxweb2-ui/vite.config.ts
+++ packages/pxweb2-ui/vite.config.ts
@@ -43,4 +43,19 @@ export default defineConfig({
       external: ['react', 'react-dom', 'react/jsx-runtime'],
     },
   },
+
+  test: {
+    globals: true,
+    cache: {
+      dir: '../../node_modules/.vitest',
+    },
+    environment: 'jsdom',
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+
+    reporters: ['default'],
+    coverage: {
+      reportsDirectory: '../../coverage/libs/pxweb2-ui',
+      provider: 'v8',
+    },
+  },
 });
