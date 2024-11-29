46bf8d5717992238132cd47d3d7fa45b69d4e23c
Michael Pande <git@e2e.no>
2024-11-29 12:50:59 +0100

    Remove most of the remaining nx residue from project

--- packages/pxweb2/vite.config.ts
+++ packages/pxweb2/vite.config.ts
@@ -19,11 +19,6 @@ export default defineConfig({
 
   plugins: [react()],
 
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [ nxViteTsPaths() ],
-  // },
-
   build: {
     outDir: './dist/',
     reportCompressedSize: true,
