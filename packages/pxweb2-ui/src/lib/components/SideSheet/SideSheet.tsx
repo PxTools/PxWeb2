4a20d8d91472b84add53cd3d16416dc61b3517cf
MikaelNordberg <mikael.nordberg@scb.se>
2025-01-16 09:59:07 +0100

    Made SideSheet a dialog

--- packages/pxweb2-ui/src/lib/components/SideSheet/SideSheet.tsx
+++ packages/pxweb2-ui/src/lib/components/SideSheet/SideSheet.tsx
@@ -1,4 +1,5 @@
 import cl from 'clsx';
+import { useEffect, useRef, useState } from 'react';
 
 import classes from './SideSheet.module.scss';
 import Button from '../Button/Button';
@@ -17,15 +18,33 @@ export function SideSheet({
   children,
 }: SideSheetProps) {
   const cssClasses = className.length > 0 ? ' ' + className : '';
+  const [isSideSheetOpen, setIsSideSheetOpen] = useState(isOpen);
+  const sideSheetRef = useRef<HTMLDialogElement | null>(null);
+
+  useEffect(() => {
+    setIsSideSheetOpen(isOpen);
+  }, [isOpen]);
+
+  useEffect(() => {
+    const modalElement = sideSheetRef.current;
+    if (modalElement) {
+      if (isSideSheetOpen) {
+        modalElement.showModal();
+      } else {
+        modalElement.close();
+      }
+    }
+  }, [isSideSheetOpen]);
 
   const handleCloseSideSheet = () => {
     if (onClose) {
       onClose();
     }
+    setIsSideSheetOpen(false); // Ensure that the modal's state is updated when it's closed
   };
 
   return (
-    <div className={cl(classes.sideSheet) + cssClasses}>
+    <dialog ref={sideSheetRef} className={cl(classes.sideSheet) + cssClasses}>
       <span>SideSheet</span>
       <Button
         variant="tertiary"
@@ -34,8 +53,8 @@ export function SideSheet({
         onClick={() => handleCloseSideSheet()}
       ></Button>
       <div>{children}</div>
-      {isOpen && <span>SideSheet is open</span>}
-    </div>
+      {isSideSheetOpen && <span>SideSheet is open</span>}
+    </dialog>
   );
 }
 
