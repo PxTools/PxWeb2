cd992ea9fbeb2331c75b4ba654a882b0b3ec992c
MikaelNordberg <mikael.nordberg@scb.se>
2025-01-17 15:47:46 +0100

    Added tabs for information

--- packages/pxweb2/src/app/components/ContentTop/ContentTop.tsx
+++ packages/pxweb2/src/app/components/ContentTop/ContentTop.tsx
@@ -12,6 +12,10 @@ import {
   Link,
   SideSheet,
   PxTable,
+  TabsProvider,
+  Tabs,
+  Tab,
+  TabPanel,
 } from '@pxweb2/pxweb2-ui';
 
 export interface ContenetTopProps {
@@ -22,6 +26,7 @@ export interface ContenetTopProps {
 export function ContentTop({ pxtable, staticTitle }: ContenetTopProps) {
   const { t } = useTranslation();
   const [isSideSheetOpen, setIsSideSheetOpen] = useState<boolean>(false);
+  const [activeTab, setActiveTab] = useState('px-tab1');
 
   const handleOpenSideSheet = () => {
     setIsSideSheetOpen(true);
@@ -79,7 +84,42 @@ export function ContentTop({ pxtable, staticTitle }: ContenetTopProps) {
             setIsSideSheetOpen(false);
           }}
         >
-          <div>Any content</div>
+          <TabsProvider activeTab={activeTab} setActiveTab={setActiveTab}>
+            <Tabs variant="fixed">
+              <Tab
+                id="tab-footnotes"
+                label="Footnotes"
+                controls="pnl-footnotes"
+              ></Tab>
+              <Tab
+                id="tab-definitions"
+                label="Definitions"
+                controls="pnl-definitions"
+              ></Tab>
+              <Tab
+                id="tab-details"
+                label="Details"
+                controls="pnl-details"
+              ></Tab>
+              <Tab
+                id="tab-contact"
+                label="Contact"
+                controls="pnl-contact"
+              ></Tab>
+            </Tabs>
+            <TabPanel id="pnl-footnotes" controlledBy="tab-footnotes">
+              Footnotes
+            </TabPanel>
+            <TabPanel id="pnl-definitions" controlledBy="tab-definitions">
+              Definitions
+            </TabPanel>
+            <TabPanel id="pnl-details" controlledBy="tab-details">
+              Details
+            </TabPanel>
+            <TabPanel id="pnl-contact" controlledBy="tab-contact">
+              Contact
+            </TabPanel>
+          </TabsProvider>
         </SideSheet>
       )}
     </>
