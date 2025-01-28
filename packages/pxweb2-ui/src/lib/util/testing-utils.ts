eb1a23704479c2574f134f333a698e0cd1ba5c70
Sjur Sutterud Sagen <sjs@ssb.no>
2025-01-28 16:03:32 +0100

    Refactor mock for less nesting of functions

--- packages/pxweb2-ui/src/lib/util/testing-utils.ts
+++ packages/pxweb2-ui/src/lib/util/testing-utils.ts
@@ -1,18 +1,20 @@
 import { vi } from 'vitest';
 
-export const mockReactI18next = () => {
-  vi.mock('react-i18next', () => ({
-    useTranslation: () => ({
-      t: (key: string) => key,
-      i18n: {
-        // eslint-disable-next-line @typescript-eslint/no-empty-function
-        changeLanguage: () => new Promise(() => {}),
-      },
-    }),
-    initReactI18next: {
-      type: '3rdParty',
+const mockConfig = {
+  useTranslation: () => ({
+    t: (key: string) => key,
+    i18n: {
       // eslint-disable-next-line @typescript-eslint/no-empty-function
-      init: () => {},
+      changeLanguage: () => new Promise(() => {}),
     },
-  }));
+  }),
+  initReactI18next: {
+    type: '3rdParty',
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    init: () => {},
+  },
+};
+
+export const mockReactI18next = () => {
+  vi.mock('react-i18next', () => mockConfig);
 };
