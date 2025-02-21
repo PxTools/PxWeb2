84530a7e8390e6bd1a6148a503a1234889ff2809
MikaelNordberg <mikael.nordberg@scb.se>
2025-02-21 10:58:30 +0100

    JSON-stat2 mapping of more metadata at table level

--- packages/pxweb2-ui/src/lib/shared-types/pxTableMetadata.ts
+++ packages/pxweb2-ui/src/lib/shared-types/pxTableMetadata.ts
@@ -27,6 +27,46 @@ export type PxTableMetadata = {
    */
   updated: Date;
   /**
+   * The table's source.
+   */
+  source: string;
+  /**
+   * The table's info file.
+   */
+  infofile: string;
+  /**
+   * Number of decimals when displaying table data
+   */
+  decimals: number;
+  /**
+   * If the table is official statistics or not.
+   */
+  officialStatistics: boolean;
+  /**
+   * If aggregation is allowed or not
+   */
+  aggregationAllowed: boolean;
+  /**
+   * Table content
+   */
+  contents: string;
+  /**
+   * If the table description shall be used by default or not.
+   */
+  descriptionDefault: boolean;
+  /**
+   * Table matrix
+   */
+  matrix: string;
+  /**
+   * Table subject code
+   */
+  subjectCode: string;
+  /**
+   * Table subject area
+   */
+  subjectArea: string;
+  /**
    * The variables that are part of the table.
    */
   variables: Variable[];
--- packages/pxweb2/src/mappers/JsonStat2ResponseMapper.spec.ts
+++ packages/pxweb2/src/mappers/JsonStat2ResponseMapper.spec.ts
@@ -7,6 +7,9 @@ describe('JsonStat2ResponseMapper', () => {
       // Arrange
       const dataset: Dataset = {
         // Dummy data
+        label: 'Table label',
+        source: 'Statistics Sweden',
+        updated: '2025-02-21T07:00:00Z',
         dimension: {
           time: {
             label: 'Time',
@@ -84,6 +87,19 @@ describe('JsonStat2ResponseMapper', () => {
             '0': true,
             '1': true,
           },
+          px: {
+            infofile: 'TEST01',
+            tableid: 'TAB001',
+            decimals: 0,
+            'official-statistics': true,
+            aggregallowed: true,
+            language: 'en',
+            contents: 'Population',
+            descriptiondefault: false,
+            matrix: 'TEST001',
+            'subject-code': 'POP',
+            'subject-area': 'Population',
+          },
           contact: [
             {
               name: ' Contact 1',
@@ -107,6 +123,22 @@ describe('JsonStat2ResponseMapper', () => {
       // Assert
       expect(pxTable).toBeDefined();
       // Add more assertions here to validate the mapping
+      expect(pxTable.metadata.label).equals('Table label');
+      expect(pxTable.metadata.source).equals('Statistics Sweden');
+      expect(pxTable.metadata.updated).toEqual(
+        new Date('2025-02-21T07:00:00Z'),
+      );
+      expect(pxTable.metadata.decimals).equals(0);
+      expect(pxTable.metadata.officialStatistics).equals(true);
+      expect(pxTable.metadata.aggregationAllowed).equals(true);
+      expect(pxTable.metadata.contents).equals('Population');
+      expect(pxTable.metadata.descriptionDefault).equals(false);
+      expect(pxTable.metadata.matrix).equals('TEST001');
+      expect(pxTable.metadata.id).equals('TAB001');
+      expect(pxTable.metadata.language).equals('en');
+      expect(pxTable.metadata.infofile).equals('TEST01');
+      expect(pxTable.metadata.subjectCode).equals('POP');
+      expect(pxTable.metadata.subjectArea).equals('Population');
       expect(pxTable.metadata.variables.length).equals(2);
       expect(pxTable.metadata.variables[0].values.length).equals(2);
       expect(pxTable.metadata.variables[0].mandatory).equals(true);
