e1f552a51a2171401edaa7ebf44b1a2bd399ed1d
Sjur Sutterud Sagen <sjs@ssb.no>
2025-02-21 15:02:46 +0100

    Add some basic tests to search

--- packages/pxweb2-ui/src/lib/components/Search/Search.spec.tsx
+++ packages/pxweb2-ui/src/lib/components/Search/Search.spec.tsx
@@ -1,10 +1,49 @@
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import Search from './Search';
 
 describe('Search', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Search />);
+    const { baseElement } = render(<Search variant="default" />);
+
     expect(baseElement).toBeTruthy();
+  });
+
+  it('should call onChange when typing', async () => {
+    const user = userEvent.setup();
+    const handleChange = vi.fn();
+    const { getByRole } = render(
+      <Search variant="default" onChange={handleChange} />,
+    );
+    const input = getByRole('textbox');
+
+    await user.type(input, 'hello');
+
+    expect(handleChange).toHaveBeenCalledWith('h');
+    expect(handleChange).toHaveBeenCalledWith('hello');
+  });
+
+  it('should display label when showLabel is true', () => {
+    const { getByText } = render(
+      <Search variant="default" showLabel labelText="Search label" />,
+    );
+
+    expect(document.body.contains(getByText('Search label'))).toBe(true);
+  });
+
+  it('should clear input and refocus when clear button is clicked', async () => {
+    const user = userEvent.setup();
+    const handleChange = vi.fn();
+    const { getByRole } = render(
+      <Search variant="default" onChange={handleChange} value="test" />,
+    );
+    const input = getByRole('textbox');
+    const button = getByRole('button');
+
+    await user.click(button);
+
+    expect(handleChange).toHaveBeenCalledWith('');
+    expect(document.activeElement).toBe(input);
   });
 });
