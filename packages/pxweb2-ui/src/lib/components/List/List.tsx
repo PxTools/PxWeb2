7e6233ce55b4d23d2fd3eaedf92b23deb2b1ab9c
PerIngeVaaje <per.inge.vaaje@ssb.no>
2025-02-24 15:32:11 +0100

    Code cleanup

--- packages/pxweb2-ui/src/lib/components/List/List.tsx
+++ packages/pxweb2-ui/src/lib/components/List/List.tsx
@@ -9,37 +9,23 @@ interface ListProps {
   listType: 'ul' | 'ol';
   listGroup?: boolean;
   nested?: boolean;
-  // listSubType?: 'nested' | 'listgroup' | 'default';
   children: React.ReactNode;
 }
 
 export function List({
   heading,
   subHeading,
-  listType: listType = 'ol',
+  listType = 'ol',
   listGroup = false,
   nested = false,
-  // listSubType = 'default',
   children,
-}: ListProps) {
+}: Readonly<ListProps>) {
   const listclassextension = listGroup ? '-listgroup' : '';
   if (nested) {
     heading = undefined;
     subHeading = undefined;
     listGroup = false;
   }
-
-  // let listclassextension = '';
-  // switch (listSubType) {
-  //   case 'nested':
-  //     listclassextension = '-nested';
-  //     break;
-  //   case 'listgroup':
-  //     listclassextension = '-listgroup';
-  //     break;
-  //   default:
-  //     break;
-  // }
 
   return (
     <div className={cl(classes[`list-component-wrapper`])}>
@@ -48,7 +34,6 @@ export function List({
           <Heading size={'small'}>{heading}</Heading>
         </div>
       )}
-      {/* {listSubType !== 'listgroup' && subHeading && ( */}
       {!listGroup && subHeading && (
         <div className={cl(classes[`sub-heading-wrapper`])}>
           {<Heading size={'xsmall'}>{subHeading}</Heading>}
