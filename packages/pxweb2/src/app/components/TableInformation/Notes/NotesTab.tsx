44b04be194947a2bffdc304907fd05477a304b7e
MikaelNordberg <mikael.nordberg@scb.se>
2025-02-27 15:03:27 +0100

    Implemented display of "No notes"

--- packages/pxweb2/src/app/components/TableInformation/Notes/NotesTab.tsx
+++ packages/pxweb2/src/app/components/TableInformation/Notes/NotesTab.tsx
@@ -1,7 +1,7 @@
 import cl from 'clsx';
 
 import classes from './NotesTab.module.scss';
-import { PxTable } from '@pxweb2/pxweb2-ui';
+import { NoNotes, PxTable } from '@pxweb2/pxweb2-ui';
 
 export type NotesTabProps = {
   readonly pxtable: PxTable | undefined;
@@ -11,6 +11,7 @@ export function NotesTab({ pxtable }: NotesTabProps) {
   return (
     <div className={cl(classes.notesTab)}>
       {pxtable?.metadata.notes[0].text}
+      <NoNotes tableLevel={false} />
     </div>
   );
 }
