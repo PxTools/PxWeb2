d362a3c9a6a8a78132dfdac972dcb479793d7f1b
MikaelNordberg <mikael.nordberg@scb.se>
2025-02-27 15:04:20 +0100

    Prettier code

--- packages/pxweb2-ui/src/lib/components/Notes/NoNotes.tsx
+++ packages/pxweb2-ui/src/lib/components/Notes/NoNotes.tsx
@@ -12,14 +12,16 @@ export function NoNotes({ tableLevel }: NoNotesProps) {
   const heading = t(
     'presentation_page.main_content.about_table.footnotes.missing_heading',
   );
-  const itemText = tableLevel ? t(
-    'presentation_page.main_content.about_table.footnotes.missing_text_table',
-  ) : t(
-    'presentation_page.main_content.about_table.footnotes.missing_text_selection'
-  );
+  const itemText = tableLevel
+    ? t(
+        'presentation_page.main_content.about_table.footnotes.missing_text_table',
+      )
+    : t(
+        'presentation_page.main_content.about_table.footnotes.missing_text_selection',
+      );
 
   return (
-    <List heading={heading} listType="ul" >
+    <List heading={heading} listType="ul">
       <ListItem>{itemText}</ListItem>
     </List>
   );
