1cc4f7b0ed70e84f5f16796d13c8bd226607b364
Kent Mossbäck <kent.mossback@scb.se>
2025-03-10 17:26:56 +0100

    Refactor ErrorPage component for improved code consistency and formatting

--- packages/pxweb2/src/app/components/ErrorPage/ErrorPage.tsx
+++ packages/pxweb2/src/app/components/ErrorPage/ErrorPage.tsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import { Alert } from '@pxweb2/pxweb2-ui';
-import { useRouteError } from 'react-router'
+import { useRouteError } from 'react-router';
 
 type RouteError = {
-  statusText?: string,
-  message?: string,
-}
+  statusText?: string;
+  message?: string;
+};
 
 export const ErrorPage: React.FC = () => {
-  const error = useRouteError() as RouteError | null
-  console.log(error)
+  const error = useRouteError() as RouteError | null;
+  console.log(error);
 
-return (
-        <Alert variant="error" size="small">
-          {error?.statusText || error?.message }
-        </Alert>
-       );
+  return (
+    <Alert variant="error" size="small">
+      {error?.statusText || error?.message}
+    </Alert>
+  );
 };
 
 export default ErrorPage;
