4a0fea54f8e14a93084d3a622b29477cee1acac7
oyessb <47382101+oyessb@users.noreply.github.com>
2025-03-10 16:22:06 +0100

    Add Details and DetailsTab components for use in details tab.

---
+++ packages/pxweb2/src/app/components/TableInformation/Details/Details.tsx
@@ -0,0 +1,52 @@
+import cl from 'clsx';
+import { useTranslation } from 'react-i18next';
+
+import classes from './Details.module.scss';
+import { Heading, Icon, IconProps, BodyShort } from '@pxweb2/pxweb2-ui';
+import { ContentDetails } from './utils';
+
+export type DetailsProps = {
+  readonly heading: string;
+  readonly icon: IconProps['iconName'];
+  readonly type?: 'text' | 'boolean';
+  readonly value?: string | boolean;
+  readonly children?: ContentDetails[];
+};
+
+export function Details({
+  heading,
+  icon,
+  type = 'text',
+  value,
+  children,
+}: DetailsProps) {
+  const { t } = useTranslation();
+  return (
+    <div className={cl(classes.detail)}>
+      <div className={cl(classes.iconWrapper)}>
+        <Icon iconName={icon} />
+      </div>
+      <div className={cl(classes.textContainer)}>
+        <Heading size="small">{heading}</Heading>
+        {type === 'text' && !children && <BodyShort>{value}</BodyShort>}
+        {type === 'boolean' && (
+          <BodyShort>
+            {value
+              ? t(
+                  'presentation_page.main_content.about_table.details.boolean_true',
+                )
+              : t(
+                  'presentation_page.main_content.about_table.details.boolean_false',
+                )}
+          </BodyShort>
+        )}
+        {children?.map((content) => (
+          <div className={cl(classes.contentDetail)} key={content.subHeading}>
+            <Heading size="xsmall">{content.subHeading}</Heading>
+            <BodyShort>{content.text}</BodyShort>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
