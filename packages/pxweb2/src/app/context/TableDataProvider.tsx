2babc5330b9673b502162e55a887611a7d81cc8a
oyessb <47382101+oyessb@users.noreply.github.com>
2025-03-19 13:35:22 +0100

    Refactor TableDataProvider to use OutputFormatType for table data retrieval

--- packages/pxweb2/src/app/context/TableDataProvider.tsx
+++ packages/pxweb2/src/app/context/TableDataProvider.tsx
@@ -4,6 +4,7 @@ import React, { createContext, useEffect, useState, ReactNode } from 'react';
 import useVariables from './useVariables';
 import {
   Dataset,
+  OutputFormatType,
   TableService,
   VariableSelection,
   VariablesSelection,
@@ -261,7 +262,7 @@ const TableDataProvider: React.FC<TableDataProviderProps> = ({ children }) => {
     const res = await TableService.getTableDataByPost(
       tableId,
       i18n.language,
-      'json-stat2',
+      OutputFormatType.JSON_STAT2,
       undefined,
       variablesSelection,
     );
