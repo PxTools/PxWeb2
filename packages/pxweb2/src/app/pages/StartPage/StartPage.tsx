c83d160bd9c670dad843ade7e36f2600897d5373
likp <petros.likidis@scb.se>
2025-03-25 15:25:16 +0100

    Fixed link on startpage

--- packages/pxweb2/src/app/pages/StartPage/StartPage.tsx
+++ packages/pxweb2/src/app/pages/StartPage/StartPage.tsx
@@ -3,7 +3,7 @@ const StartPage = () => {
     <div>
       <h1>Start Page</h1>
       <p>This is the start page for the application.</p>
-      <a href="/table/tab638">Go to table viewer</a>
+      <a href="/table">Go to table viewer</a>
     </div>
   );
 };
