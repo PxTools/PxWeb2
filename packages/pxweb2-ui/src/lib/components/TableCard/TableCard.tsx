ceef1a56714dd17ecf6d5b85361de60e775fa585
Carina <cgn@ssb.no>
2025-03-28 08:18:27 +0100

    Added forwardRef

--- packages/pxweb2-ui/src/lib/components/TableCard/TableCard.tsx
+++ packages/pxweb2-ui/src/lib/components/TableCard/TableCard.tsx
@@ -1,4 +1,4 @@
-import React, { type ReactNode } from 'react';
+import React, { type ReactNode, forwardRef } from 'react';
 import cl from 'clsx';
 import styles from './TableCard.module.scss';
 import { Icon } from '../Icon/Icon';
@@ -15,63 +15,64 @@ interface TableCardProps {
   status?: 'active' | 'closed';
 }
 
-export const TableCard: React.FC<TableCardProps> = ({
-  href,
-  icon,
-  tableId,
-  title,
-  period,
-  frequency,
-  updatedLabel,
-  lastUpdated,
-  status = 'active',
-}) => (
-  <div className={cl(styles.tableCard)}>
-    {icon && (
-      <div className={cl(styles.iconWrapper, styles[status])}>{icon}</div>
-    )}
-    <div className={cl(styles.cardContent)}>
-      <div className={cl(styles.titleWrapper)}>
-        <a
-          href={href}
-          className={cl(
-            styles.title,
-            styles['heading-small'],
-            styles['no_underline'],
-          )}
-        >
-          {title}
-        </a>
-      </div>
-      <div className={cl(styles.tableMeta)}>
-        <div className={cl(styles.timeWrapper)}>
-          {period && (
-            <span className={cl(styles.period, styles['heading-xsmall'])}>
-              {period}
-            </span>
-          )}
-          {frequency && (
-            <span className={cl(styles.frequency, styles['label-small'])}>
-              {frequency}
-            </span>
-          )}
-          {lastUpdated && (
-            <div className={cl(styles.lastUpdated)}>
-              <Icon iconName="Clock" />
-              <span className={cl(styles['bodyshort-small'])}>
-                {updatedLabel} {lastUpdated}
+export const TableCard = forwardRef<HTMLAnchorElement, TableCardProps>(
+  (
+    {
+      href,
+      icon,
+      tableId,
+      title,
+      period,
+      frequency,
+      updatedLabel,
+      lastUpdated,
+      status = 'active',
+    },
+    ref,
+  ) => (
+    <div className={cl(styles.tableCard)}>
+      {icon && (
+        <div className={cl(styles.iconWrapper, styles[status])}>{icon}</div>
+      )}
+      <div className={cl(styles.cardContent)}>
+        <div className={cl(styles.titleWrapper)}>
+          <a
+            href={href}
+            ref={ref}
+            className={cl(styles.title, styles['heading-small'])}
+          >
+            {title}
+          </a>
+        </div>
+        <div className={cl(styles.tableMeta)}>
+          <div className={cl(styles.timeWrapper)}>
+            {period && (
+              <span className={cl(styles.period, styles['heading-xsmall'])}>
+                {period}
               </span>
-            </div>
+            )}
+            {frequency && (
+              <span className={cl(styles.frequency, styles['label-small'])}>
+                {frequency}
+              </span>
+            )}
+            {lastUpdated && (
+              <div className={cl(styles.lastUpdated)}>
+                <Icon iconName="Clock" />
+                <span className={cl(styles['bodyshort-small'])}>
+                  {updatedLabel} {lastUpdated}
+                </span>
+              </div>
+            )}
+          </div>
+          {tableId && (
+            <span className={cl(styles.tableId, styles['label-small'])}>
+              {tableId}
+            </span>
           )}
         </div>
-        {tableId && (
-          <span className={cl(styles.tableId, styles['label-small'])}>
-            {tableId}
-          </span>
-        )}
       </div>
     </div>
-  </div>
+  ),
 );
-
 export default TableCard;
