1e83ab9772f7cfb9e95af61cafcea8d711135d34
MikaelNordberg <mikael.nordberg@scb.se>
2025-04-28 16:05:09 +0200

    Prettier code

--- packages/pxweb2-ui/src/lib/components/Notes/SymbolExplanationNotes.spec.tsx
+++ packages/pxweb2-ui/src/lib/components/Notes/SymbolExplanationNotes.spec.tsx
@@ -6,20 +6,20 @@ import { SymbolExplanationNotes } from './SymbolExplanationNotes';
 import { dummyNotes } from './notesDummyData';
 
 describe('SymbolExplanationNotes', () => {
+  it('renders a single note correctly when there is only one note', () => {
+    const singleNote = { A: 'Single note content' };
+    render(<SymbolExplanationNotes notes={singleNote} />);
+    const noteContent = screen.getByText('Single note content');
+    expect(noteContent).toBeInTheDocument();
+  });
 
-    it('renders a single note correctly when there is only one note', () => {
-        const singleNote = { A: 'Single note content' };
-        render(<SymbolExplanationNotes notes={singleNote} />);
-        const noteContent = screen.getByText('Single note content');
-        expect(noteContent).toBeInTheDocument();
+  it('renders multiple notes correctly when there are multiple notes', () => {
+    render(
+      <SymbolExplanationNotes notes={dummyNotes.SymbolExplanationNotes} />,
+    );
+    Object.values(dummyNotes.SymbolExplanationNotes).forEach((note) => {
+      const noteContent = screen.getByText(note);
+      expect(noteContent).toBeInTheDocument();
     });
-
-    it('renders multiple notes correctly when there are multiple notes', () => {
-        render(<SymbolExplanationNotes notes={dummyNotes.SymbolExplanationNotes} />);
-        Object.values(dummyNotes.SymbolExplanationNotes).forEach((note) => {
-            const noteContent = screen.getByText(note);
-            expect(noteContent).toBeInTheDocument();
-        });
-    });
-
+  });
 });
