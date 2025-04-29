894357cd5f6eda993d5f6af7ee484b2db137a3b1
MikaelNordberg <mikael.nordberg@scb.se>
2025-04-29 10:16:28 +0200

    MandatoryTableNotes unit tests

--- packages/pxweb2-ui/src/lib/components/Notes/NoNotes.spec.tsx
+++ packages/pxweb2-ui/src/lib/components/Notes/NoNotes.spec.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import { describe, it, expect } from 'vitest';
 import '@testing-library/jest-dom/vitest';
+
 import { NoNotes } from './NoNotes';
 
 describe('NoNotes', () => {
