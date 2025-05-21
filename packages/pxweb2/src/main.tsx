eb201276e9209aa08a22fd8aadb7d3f08466d474
Sjur Sutterud Sagen <sjs@ssb.no>
2025-05-21 13:56:11 +0200

    Remove outdated routing TODO comments and console log from main.tsx

--- packages/pxweb2/src/main.tsx
+++ packages/pxweb2/src/main.tsx
@@ -15,30 +15,6 @@ if (location.href.includes('localhost')) {
   validateConfig();
 }
 
-/**
- * TODO:
- * Routing:
- * The routing works as expected when default language is in the URL
- * - changes correctly to new, supported language DONE
- * - shows language not supported when language part in URL does not match a supported language DONE
- * - REDIRECT to default language when no language in url, does not work
- *
- * The routing works as expected when default language is not in the URL
- * - correctly shows default language when it is NOT in the URL DONE
- * - correctly shows new, supported language when it is in the URL DONE
- * - shows language not supported when no language part in URL, or language part in URL does not match a supported language DONE
- *
- * Routing:
- * - Add NotFound component
- *   - Make it take in language text
- *
- * Code:
- * - Move the routing to a separate file
- * - Tests?
- */
-
-console.log('router', router);
-
 root.render(
   <StrictMode>
     <AppProvider>
