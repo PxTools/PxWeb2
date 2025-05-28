c5a99d59ad507454a754c07a10532cb2704f88d2
Sjur Sutterud Sagen <sjs@ssb.no>
2025-05-28 14:02:31 +0200

    Add ContentBox component
    This adds the contentbox component to the library, which we can use to
    wrap the different operations we are going to have in the sidebar in the
    web app.
    
    I am a bit unsure if it should be added to the web app components, or
    be added in the ui library like I have done.
    
    Also added some basic tests and stories, and updated the
    NavigationDrawer to better fit the intended use.

---
+++ packages/pxweb2-ui/src/lib/components/ContentBox/ContentBox.tsx
@@ -0,0 +1,26 @@
+import { ReactNode } from 'react';
+import cl from 'clsx';
+
+import styles from './ContentBox.module.scss';
+
+interface ContentBoxProps {
+  title?: string;
+  children: ReactNode;
+}
+
+export function ContentBox({ title, children }: ContentBoxProps) {
+  return (
+    <div className={cl(styles.contentBox)}>
+      {title && (
+        <div className={cl(styles.title)}>
+          <h3 className={cl(styles.titleText, styles[`label-small`])}>
+            {title}
+          </h3>
+        </div>
+      )}
+      {children}
+    </div>
+  );
+}
+
+ContentBox.displayName = 'ContentBox';
