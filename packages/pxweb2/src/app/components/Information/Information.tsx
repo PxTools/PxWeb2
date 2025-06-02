288e3e5fa808aa6a477fe5ba69e5edf86fd1c260
Carina <cgn@ssb.no>
2025-06-02 13:06:57 +0200

    Fixed filter and search breakpoint

--- packages/pxweb2/src/app/components/Information/Information.tsx
+++ packages/pxweb2/src/app/components/Information/Information.tsx
@@ -1,16 +1,15 @@
-import cl from 'clsx';
 import { useTranslation } from 'react-i18next';
 
-import { Ingress } from '@pxweb2/pxweb2-ui';
+import { Heading, Ingress } from '@pxweb2/pxweb2-ui';
 import styles from './Information.module.scss';
 
 export const Information = () => {
   const { t } = useTranslation();
   return (
     <div className={styles.information}>
-      <div className={cl(styles['heading-large'])}>
+      <Heading size="large" level="1" className={styles.title}>
         {t('start_page.header')}
-      </div>
+      </Heading>
       <Ingress>{t('start_page.ingress')}</Ingress>
     </div>
   );
