13ba37068f8d9d0081e155910d3949696ba4e6b9
oyessb <47382101+oyessb@users.noreply.github.com>
2025-06-03 14:52:13 +0200

    Add legend support to Radio component (#687)
    * Add legend support to Radio component
    
    * Update Radio component to require legend prop
    
    * Add tests for legend visibility based on hideLegend prop in Radio component
    
    * Use full width for fieldset

--- packages/pxweb2-ui/src/lib/components/Radio/Radio.tsx
+++ packages/pxweb2-ui/src/lib/components/Radio/Radio.tsx
@@ -15,34 +15,52 @@ export interface RadioProps
   options: SelectOption[];
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
   selectedOption?: string;
+  legend: string;
+  hideLegend?: boolean;
 }
 
 export const Radio = forwardRef<HTMLInputElement, Readonly<RadioProps>>(
-  ({ variant = 'default', name, options, onChange, selectedOption }, ref) => {
+  (
+    {
+      variant = 'default',
+      name,
+      options,
+      onChange,
+      selectedOption,
+      legend,
+      hideLegend = true,
+    },
+    ref,
+  ) => {
     return (
-      <div className={cl(classes.radioGroup)}>
-        {options.map((option) => (
-          <label
-            className={cl(classes.container, classes[`bodyshort-medium`])}
-            key={option.value}
-          >
-            <div className={cl(classes[variant], classes.divider)}>
-              <input
-                className={cl(classes[variant])}
-                type="radio"
-                id={option.value}
-                name={name}
-                value={option.value}
-                key={option.value}
-                onChange={onChange}
-                checked={option.value === selectedOption}
-                ref={option.value === selectedOption ? ref : null}
-              />
-              {option.label}
-            </div>
-          </label>
-        ))}
-      </div>
+      <fieldset className={cl(classes.fieldset)}>
+        <legend className={cl({ [classes.legendSrOnly]: hideLegend })}>
+          {legend}
+        </legend>
+        <div className={cl(classes.radioGroup)}>
+          {options.map((option) => (
+            <label
+              className={cl(classes.container, classes[`bodyshort-medium`])}
+              key={option.value}
+            >
+              <div className={cl(classes[variant], classes.divider)}>
+                <input
+                  className={cl(classes[variant])}
+                  type="radio"
+                  id={option.value}
+                  name={name}
+                  value={option.value}
+                  key={option.value}
+                  onChange={onChange}
+                  checked={option.value === selectedOption}
+                  ref={option.value === selectedOption ? ref : null}
+                />
+                {option.label}
+              </div>
+            </label>
+          ))}
+        </div>
+      </fieldset>
     );
   },
 );
