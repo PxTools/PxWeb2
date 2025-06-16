21ac19b4aef46189afbba0348e343fb37e860470
Kent Mossbäck <kent.mossback@scb.se>
2025-06-16 11:35:47 +0200

    test(ActionItemIcon): Add aria-label test and clean up imports

--- packages/pxweb2-ui/src/lib/components/Icon/ActionItemIcon.spec.tsx
+++ packages/pxweb2-ui/src/lib/components/Icon/ActionItemIcon.spec.tsx
@@ -1,6 +1,7 @@
 import { render } from '@testing-library/react';
-
 import { ActionItemIcon } from './ActionItemIcon';
+import { describe, it, expect} from 'vitest';
+import '@testing-library/jest-dom/vitest';
 
 describe('ActionItemIcon', () => {
   it('should render successfully', () => {
@@ -8,10 +9,16 @@ describe('ActionItemIcon', () => {
     expect(baseElement).toBeTruthy();
   });
   it('should return null if icon does not exist', () => {
-    // @ts-expect-error: purposely passing invalid icon name
     const { container } = render(
       <ActionItemIcon largeIconName="NonExistentIcon" />,
     );
     expect(container.firstChild).toBeNull();
+  });
+  it('should set the aria-label attribute when provided', () => {
+    const ariaLabel = 'Test Icon';
+    const { getByLabelText } = render(
+      <ActionItemIcon largeIconName="Table" ariaLabel={ariaLabel} />
+    );
+    expect(getByLabelText(ariaLabel)).toBeInTheDocument();
   });
 });
