5a001c0537b96bdd1704498fdf249d9f4a2ced71
MikaelNordberg <mikael.nordberg@scb.se>
2025-06-24 08:32:06 +0200

    Use texts instead of codes when exporting to CSV

--- packages/pxweb2/src/app/util/export/exportUtil.ts
+++ packages/pxweb2/src/app/util/export/exportUtil.ts
@@ -26,6 +26,7 @@ export async function exportToFile(
       outputFormatParams = [
         OutputFormatParamType.SEPARATOR_SEMICOLON,
         OutputFormatParamType.INCLUDE_TITLE,
+        OutputFormatParamType.USE_TEXTS
       ];
       fileExtension = 'csv';
       break;
