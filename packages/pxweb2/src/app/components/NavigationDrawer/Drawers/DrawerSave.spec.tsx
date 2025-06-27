177d33f029e284389eb16a6fc1d84456185f495c
Sjur Sutterud Sagen <sjs@ssb.no>
2025-06-27 10:45:46 +0200

    Update DrawerSave test

--- packages/pxweb2/src/app/components/NavigationDrawer/Drawers/DrawerSave.spec.tsx
+++ packages/pxweb2/src/app/components/NavigationDrawer/Drawers/DrawerSave.spec.tsx
@@ -1,17 +1,13 @@
 import { describe, it, expect } from 'vitest';
-import { render } from '@testing-library/react';
+import { renderWithProviders } from '../../../util/testing-utils';
 import '@testing-library/jest-dom/vitest';
 
 import { DrawerSave } from './DrawerSave';
 
 describe('DrawerSave', () => {
   it('renders successfully', () => {
-    const element = render(<DrawerSave />);
+    const element = renderWithProviders(<DrawerSave tableId="test" />);
 
     expect(element).toBeTruthy();
-  });
-
-  it('has correct display name', () => {
-    expect(DrawerSave.displayName).toBe('DrawerSave');
   });
 });
