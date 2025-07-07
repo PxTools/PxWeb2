cb5f761e03a2816614c7cf0d1f195fd96c32a0a4
Sjur Sutterud Sagen <sjs@ssb.no>
2025-07-07 13:01:48 +0200

    Add support for translations in Storybook/UI-lib
    This adds a first iteration of language support to the Storybook in the
    PxWeb2 UI library. The solution was made following Storybooks example
    for this, but I also made considerations for speed and reducing
    complexity.
    
    Therefore, there are improvements that can be made to this. For
    instance, we could consider moving the translation into their own
    package, and import them into other packages as needed. Now it just
    imports directly from the web app.

---
+++ packages/pxweb2-ui/i18n/config.ts
@@ -0,0 +1,20 @@
+import i18n from 'i18next';
+import Backend from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+i18n
+  .use(Backend) // lazy loads translations from /public/locales
+  .use(LanguageDetector) // detect user language
+  .init({
+    fallbackLng: 'en',
+    supportedLngs: ['en', 'no', 'se', 'ar'],
+    debug: true,
+    interpolation: {
+      escapeValue: false,
+    },
+    detection: {
+      caches: [], // Do not cache the language in local storage or cookies.
+    },
+  });
+
+export default i18n;
