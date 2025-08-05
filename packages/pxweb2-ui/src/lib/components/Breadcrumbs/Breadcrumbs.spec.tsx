c3c6216e0a5263c800d1e7f4c9f8e28e644b15fe
Kent Mossbäck <kent.mossback@scb.se>
2025-08-05 15:06:17 +0200

    Clean up Breadcrumbs component test by removing unnecessary whitespace

--- packages/pxweb2-ui/src/lib/components/Breadcrumbs/Breadcrumbs.spec.tsx
+++ packages/pxweb2-ui/src/lib/components/Breadcrumbs/Breadcrumbs.spec.tsx
@@ -3,14 +3,10 @@ import { render, screen } from '@testing-library/react';
 import Breadcrumbs from './Breadcrumbs';
 
 describe('Breadcrumbs', () => {
-
-
   it('should render successfully', () => {
-    const { baseElement } = render(<Breadcrumbs  />);
+    const { baseElement } = render(<Breadcrumbs />);
     expect(baseElement).toBeTruthy();
   });
-
-
 
   // it('renders children as breadcrumb items', () => {
   //   render(
