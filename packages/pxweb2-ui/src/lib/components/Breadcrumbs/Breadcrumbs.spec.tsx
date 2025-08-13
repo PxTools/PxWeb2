33f533f099fea6b427e3d9c1540ba5ed0931e0b3
Kent Mossbäck <kent.mossback@scb.se>
2025-08-13 09:35:17 +0200

    Refactor Breadcrumbs tests: clean up import statements and improve formatting

--- packages/pxweb2-ui/src/lib/components/Breadcrumbs/Breadcrumbs.spec.tsx
+++ packages/pxweb2-ui/src/lib/components/Breadcrumbs/Breadcrumbs.spec.tsx
@@ -1,11 +1,8 @@
 //import { describe, it, expect, vi, beforeEach } from 'vitest';
-import { describe, it, expect} from 'vitest';
+import { describe, it, expect } from 'vitest';
 import { render, screen, fireEvent } from '@testing-library/react';
 import Breadcrumbs, { BreadcrumbItem } from './Breadcrumbs';
 import '@testing-library/jest-dom/vitest';
-
-
-
 
 describe('Breadcrumbs', () => {
   const items = [
