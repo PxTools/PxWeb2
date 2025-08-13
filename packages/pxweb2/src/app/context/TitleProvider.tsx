3ba4b2d32c67e5b0325d898f6c54bb0fab3387ca
oyessb <47382101+oyessb@users.noreply.github.com>
2025-08-13 14:59:17 +0200

    Formatting

--- packages/pxweb2/src/app/context/TitleProvider.tsx
+++ packages/pxweb2/src/app/context/TitleProvider.tsx
@@ -1,13 +1,17 @@
-import {useState, createContext } from 'react';
+import { useState, createContext } from 'react';
 
 export type TitleContextType = {
   title: string;
   setTitle: (title: string) => void;
 };
 
-export const TitleContext = createContext<TitleContextType | undefined>(undefined);
+export const TitleContext = createContext<TitleContextType | undefined>(
+  undefined,
+);
 
-export const TitleProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+export const TitleProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
   const [title, setTitle] = useState<string>('');
   return (
     <TitleContext.Provider value={{ title, setTitle }}>
