3a845542670066887d741ea3a0c681a9ea90a69a
Kent Mossbäck <kent.mossback@scb.se>
2025-08-21 16:48:41 +0200

    Refactor Breadcrumbs stories: update breadcrumb item text for clarity and add new story variations for long text scenarios

--- packages/pxweb2-ui/src/lib/components/Breadcrumbs/Breadcrumbs.stories.tsx
+++ packages/pxweb2-ui/src/lib/components/Breadcrumbs/Breadcrumbs.stories.tsx
@@ -43,7 +43,7 @@ export const DeaultVersion: StoryObj<typeof Breadcrumbs> = {
         new BreadcrumbItem('Statistical database', '/'),
         new BreadcrumbItem('Living conditions', '/level2'),
         new BreadcrumbItem(
-          'Children and their Families bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla',
+          'Children and their Families ',
           '/level2/level3',
         ),
         new BreadcrumbItem('Families', '/level2/level3/level4'),
@@ -56,6 +56,34 @@ export const DeaultVersion: StoryObj<typeof Breadcrumbs> = {
   ),
 };
 
+export const DeaultVersionLongTexts: StoryObj<typeof Breadcrumbs> = {
+  render: () => (
+    <Breadcrumbs
+      variant="default"
+      breadcrumbItems={[
+        new BreadcrumbItem(
+          'Statistical database',
+          '/'),
+        new BreadcrumbItem(
+          'Housing, construction and building',
+          '/level2'),
+        new BreadcrumbItem(
+          'Charges/rents for newly constructed dwellings',
+          '/level2/level3',
+        ),
+        new BreadcrumbItem(
+          'Rented dwellings and tenant-owned dwellings. Old tables – not updated. Year 2014-2022',
+          '/level2/level3/level4'),
+        new BreadcrumbItem(
+          'Rents in newly constructed buildings (rented dwellings) by region, investor and dwelling type. Year 2014 - 2022',
+          '/level2/level3/level4/level5',
+        ),
+      ]}
+    />
+  ),
+};
+
+
 export const CompactVersion: StoryObj<typeof Breadcrumbs> = {
   render: () => (
     <Breadcrumbs
@@ -67,6 +95,34 @@ export const CompactVersion: StoryObj<typeof Breadcrumbs> = {
         new BreadcrumbItem('Families', '/level2/level3/level4'),
         new BreadcrumbItem(
           'Families with children and young persons aged 0-21 living at home by region, age of children, type of family and number of children. Year 2023 - 2024',
+          '/level2/level3/level4/level5',
+        ),
+      ]}
+    />
+  ),
+};
+
+
+export const CompactVersionLongTexts: StoryObj<typeof Breadcrumbs> = {
+  render: () => (
+    <Breadcrumbs
+      variant="default"
+      breadcrumbItems={[
+        new BreadcrumbItem(
+          'Statistical database',
+          '/'),
+        new BreadcrumbItem(
+          'Housing, construction and building',
+          '/level2'),
+        new BreadcrumbItem(
+          'Charges/rents for newly constructed dwellings',
+          '/level2/level3',
+        ),
+        new BreadcrumbItem(
+          'Rented dwellings and tenant-owned dwellings. Old tables – not updated. Year 2014-2022',
+          '/level2/level3/level4'),
+        new BreadcrumbItem(
+          'Rents in newly constructed buildings (rented dwellings) by region, investor and dwelling type. Year 2014 - 2022',
           '/level2/level3/level4/level5',
         ),
       ]}
