af4272753e3ea207720a937b334555e62c216d98
Kent Mossbäck <kent.mossback@scb.se>
2025-08-25 16:46:04 +0200

    Refactor Breadcrumbs styles and component: improve indentation for better readability and maintainability

--- packages/pxweb2-ui/src/lib/components/Breadcrumbs/Breadcrumbs.tsx
+++ packages/pxweb2-ui/src/lib/components/Breadcrumbs/Breadcrumbs.tsx
@@ -97,21 +97,21 @@ export const Breadcrumbs = forwardRef<HTMLAnchorElement, BreadcrumbsProps>(
         </ul>
         {variant === 'compact' && isOverflowing && !showMore && (
           <span className={styles.compactActions}>
-          <span className={styles.dots}>{dots}</span>
-          <span
-            className={cl(styles.showMoreText)}
-            role="button"
-            tabIndex={0}
-            onClick={() => setShowMore(true)}
-            onKeyDown={(e) => {
-              if (e.key === 'Enter' || e.key === ' ') {
-                setShowMore(true);
-              }
-            }}
-            aria-label="Show more breadcrumbs"
-          >
-            Show more
-          </span>
+            <span className={styles.dots}>{dots}</span>
+            <span
+              className={cl(styles.showMoreText)}
+              role="button"
+              tabIndex={0}
+              onClick={() => setShowMore(true)}
+              onKeyDown={(e) => {
+                if (e.key === 'Enter' || e.key === ' ') {
+                  setShowMore(true);
+                }
+              }}
+              aria-label="Show more breadcrumbs"
+            >
+              Show more
+            </span>
           </span>
         )}
       </nav>
