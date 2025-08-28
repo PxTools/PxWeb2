bdb93c182a6952740cf2ee25919356de76fbb2ce
Sjur Sutterud Sagen <sjs@ssb.no>
2025-08-28 12:06:35 +0200

    Fix logo test path

--- packages/pxweb2/src/app/components/Header/Header.spec.tsx
+++ packages/pxweb2/src/app/components/Header/Header.spec.tsx
@@ -42,7 +42,7 @@ describe('Header', () => {
     ) as HTMLImageElement;
 
     expect(logo).toBeInTheDocument();
-    expect(logo).toHaveAttribute('src', '/images/main_logo/logo.svg');
+    expect(logo).toHaveAttribute('src', '/images/logo.svg');
   });
 
   it('links the logo to the language-specific root path', () => {
