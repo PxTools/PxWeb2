34cca3b58b19fa59629d19a03d545eccd87e907c
Carina <cgn@ssb.no>
2025-08-29 18:41:31 +0200

    Reverting norwegian config

--- packages/pxweb2/public/config/config.js
+++ packages/pxweb2/public/config/config.js
@@ -2,15 +2,14 @@ window.PxWeb2Config = {
   language: {
     supportedLanguages: [
       { shorthand: 'en', languageName: 'English' },
-      //{ shorthand: 'sv', languageName: 'Svenska' },
+      { shorthand: 'sv', languageName: 'Svenska' },
       { shorthand: 'no', languageName: 'Norsk' },
     ],
-    defaultLanguage: 'no',
-    fallbackLanguage: 'no',
+    defaultLanguage: 'en',
+    fallbackLanguage: 'en',
     showDefaultLanguageInPath: true,
   },  
-  apiUrl: 'https://data.qa.ssb.no/api/pxwebapi/v2-beta',
-  //apiUrl: 'https://api.scb.se/OV0104/v2beta/api/v2',
+  apiUrl: 'https://api.scb.se/OV0104/v2beta/api/v2',
   maxDataCells: 150000,
   specialCharacters: ['.', '..', ':', '-', '...', '*'],
   variableFilterExclusionList: {
