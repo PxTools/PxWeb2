014fd1da29a2907f159d3f48f76f582ae84bd8b5
oyessb <47382101+oyessb@users.noreply.github.com>
2025-09-03 13:53:45 +0200

    Add status message for drawer view

--- packages/pxweb2/src/app/components/NavigationDrawer/Drawers/DrawerView.tsx
+++ packages/pxweb2/src/app/components/NavigationDrawer/Drawers/DrawerView.tsx
@@ -1,7 +1,15 @@
-import { ContentBox } from '@pxweb2/pxweb2-ui';
+import { useTranslation } from 'react-i18next';
+
+import { ContentBox, Alert } from '@pxweb2/pxweb2-ui';
 
 export function DrawerView() {
-  return <ContentBox>View content</ContentBox>;
+  const { t } = useTranslation();
+
+  return (
+    <ContentBox>
+      <Alert variant="info">{t('common.status_messages.drawer_view')}</Alert>
+    </ContentBox>
+  );
 }
 
 DrawerView.displayName = 'DrawerView';
