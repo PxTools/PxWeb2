5f9689b6c66a391a432a8dafff5e2921943dd4a7
Carina <cgn@ssb.no>
2025-09-05 11:12:10 +0200

    Change to norwegian for testing

--- packages/pxweb2/public/config/config.js
+++ packages/pxweb2/public/config/config.js
@@ -2,14 +2,15 @@ window.PxWeb2Config = {
   language: {
     supportedLanguages: [
       { shorthand: 'en', languageName: 'English' },
-      { shorthand: 'sv', languageName: 'Svenska' },
+      //{ shorthand: 'sv', languageName: 'Svenska' },
       { shorthand: 'no', languageName: 'Norsk' },
     ],
-    defaultLanguage: 'en',
-    fallbackLanguage: 'en',
+    defaultLanguage: 'no',
+    fallbackLanguage: 'no',
     showDefaultLanguageInPath: true,
   },  
-  apiUrl: 'https://api.scb.se/OV0104/v2beta/api/v2',
+  //apiUrl: 'https://api.scb.se/OV0104/v2beta/api/v2',
+  apiUrl: 'https://data.ssb.no/api/pxwebapi/v2-beta',
   maxDataCells: 150000,
   specialCharacters: ['.', '..', ':', '-', '...', '*'],
   variableFilterExclusionList: {
