8e10bb8e35d363af69539c58ba89a0a72cd1b2c6
Kent Mossbäck <kent.mossback@scb.se>
2025-09-09 08:51:47 +0200

    Update apiUrl in config.js to use the correct endpoint

--- packages/pxweb2/public/config/config.js
+++ packages/pxweb2/public/config/config.js
@@ -9,7 +9,8 @@ window.PxWeb2Config = {
     fallbackLanguage: 'en',
     showDefaultLanguageInPath: true,
     },
-  apiUrl: 'https://api.scb.se/OV0104/v2beta',
+  //apiUrl: 'https://api.scb.se/OV0104/v2beta',
+  apiUrl: 'https://data.ssb.no/api/pxwebapi/v2-beta',
   maxDataCells: 150000,
   specialCharacters: ['.', '..', ':', '-', '...', '*'],
   variableFilterExclusionList: {
