ded9ed929e27e459306236a70f634513a81e43bb
Carina <cgn@ssb.no>
2025-09-22 09:38:20 +0200

    Pxweb2 845 implement details section (#869)

---
+++ packages/pxweb2/src/app/util/config/localeContentTypes.ts
@@ -0,0 +1,37 @@
+import type { IconProps } from '@pxweb2/pxweb2-ui';
+
+export type DetailLink = {
+  text: string;
+  url: string;
+  icon?: IconProps['iconName'];
+  iconPosition?: 'left' | 'right';
+};
+
+export type Links = {
+  header?: string;
+  items?: DetailLink[];
+};
+
+export type TextBlock = {
+  header?: string;
+  text?: string;
+};
+
+export type DetailsContent = {
+  textBlock?: TextBlock;
+  links?: Links;
+};
+
+export type DetailsSection = {
+  enabled?: boolean;
+  detailHeader?: string;
+  detailContent?: DetailsContent[];
+};
+
+export type Startpage = {
+  detailsSection?: DetailsSection;
+};
+
+export type LocaleContent = {
+  startPage?: Startpage;
+};
