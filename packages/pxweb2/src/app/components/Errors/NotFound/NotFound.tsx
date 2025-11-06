aac2f79c3e8d6ab7d0057cce8d2e97367bb83e82
PerIngeVaaje <per.inge.vaaje@ssb.no>
2025-11-06 08:49:10 +0100

    Use new function to create breadcrumbs item

--- packages/pxweb2/src/app/components/Errors/NotFound/NotFound.tsx
+++ packages/pxweb2/src/app/components/Errors/NotFound/NotFound.tsx
@@ -1,46 +1,23 @@
-import { useLocation } from 'react-router';
 import { useTranslation } from 'react-i18next';
-import { TFunction } from 'i18next';
 
-import { Breadcrumbs, BreadcrumbItem } from '@pxweb2/pxweb2-ui';
+import { Breadcrumbs } from '@pxweb2/pxweb2-ui';
 import { ErrorLayout } from '../ErrorLayout';
 import { ErrorMessage } from '../../ErrorMessage/ErrorMessage';
-import { getConfig } from '../../../util/config/getConfig';
 import useApp from '../../../context/useApp';
-
-function getBreadcrumbItems(
-  language: string,
-  t: TFunction,
-  locationPath: string = '',
-): BreadcrumbItem[] {
-  const config = getConfig();
-  const showLangInPath =
-    config.language.showDefaultLanguageInPath ||
-    language !== config.language.defaultLanguage;
-  const langPrefix = showLangInPath ? `/${language}` : '';
-
-  return [
-    // Root
-    {
-      label: t('common.breadcrumbs.breadcrumb_root_title'),
-      href: langPrefix,
-    },
-
-    // Current page
-    {
-      label: t('common.errors.not_found.title'),
-      href: `${locationPath}`,
-    },
-  ];
-}
+import { createBreadcrumbItems } from '../../../util/createBreadcrumbItems';
 
 export function NotFound() {
   const { t, i18n } = useTranslation();
   const { isTablet } = useApp();
-  const locationPath = useLocation().pathname;
-  const language = i18n.language;
+  const notFoundLabel = t('common.errors.not_found.title');
 
-  const breadcrumbItems = getBreadcrumbItems(language, t, locationPath);
+  const breadcrumbItems = createBreadcrumbItems(
+    location.pathname,
+    t,
+    i18n.language,
+    [],
+    notFoundLabel,
+  );
   const breadcrumbsVariant = isTablet ? 'compact' : 'default';
 
   return (
