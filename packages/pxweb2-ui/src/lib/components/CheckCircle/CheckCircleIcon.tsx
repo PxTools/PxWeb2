595e23a7260652b2fe13367f3b8c1d6f981f6630
oyessb <47382101+oyessb@users.noreply.github.com>
2025-11-28 16:28:19 +0100

    Add CheckCircleIcon component with styling

---
+++ packages/pxweb2-ui/src/lib/components/CheckCircle/CheckCircleIcon.tsx
@@ -0,0 +1,28 @@
+import cl from 'clsx';
+
+import styles from './CheckCircle.module.scss';
+import { Icon } from '../Icon/Icon';
+
+export interface CheckCircleIconProps {
+  checked: boolean;
+}
+
+export const CheckCircleIcon = ({
+  checked,
+}: Readonly<CheckCircleIconProps>) => {
+  return (
+    <span
+      className={cl(styles.iconWrapper, {
+        [styles['checked']]: checked,
+      })}
+    >
+      {checked && (
+        <Icon
+          iconName="CheckCircle"
+          aria-hidden="true"
+          className={styles.iconChecked}
+        ></Icon>
+      )}
+    </span>
+  );
+};
