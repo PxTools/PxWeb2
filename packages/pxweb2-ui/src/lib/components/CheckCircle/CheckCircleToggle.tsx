621708f315231318ab7293f2c98c64ec1c518786
oyessb <47382101+oyessb@users.noreply.github.com>
2025-12-10 14:31:02 +0100

    feat: Add new component CheckCircleToggle (#1028)
    * add CheckCircleToggle component
    
    * export CheckCircleToggle component in index.ts
    
    * add Storybook stories for component
    
    * add styling for CheckCircle
    
    * consolidate CheckCircleIcon tests into CheckCircle.spec.tsx and added test for CheckCircleToggle
    
    * set color for labeltext
    
    * add styling for border when focused
    
    * add text alignment to labelWrapper and center iconWrapper content
    
    * use variables for gap and border-radius
    
    * remove fixed width from checkCircle class

---
+++ packages/pxweb2-ui/src/lib/components/CheckCircle/CheckCircleToggle.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import cl from 'clsx';
+
+import styles from './CheckCircle.module.scss';
+import { CheckCircleIcon } from './CheckCircleIcon';
+
+export interface CheckCircleProps
+  extends Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, 'onChange'> {
+  checked: boolean;
+  label: string;
+  onChange?: (nextChecked: boolean) => void;
+  ref?: React.Ref<HTMLButtonElement>;
+}
+export const CheckCircleToggle = ({
+  checked,
+  label,
+  onChange,
+  onClick,
+  ref,
+  ...rest
+}: Readonly<CheckCircleProps>) => {
+  const handleClick: React.MouseEventHandler<HTMLButtonElement> = (e) => {
+    onClick?.(e);
+    if (e.defaultPrevented) {
+      return;
+    }
+    onChange?.(!checked);
+  };
+
+  return (
+    <button
+      ref={ref}
+      className={cl(styles.checkCircle, {
+        [styles.checked]: checked,
+      })}
+      role="switch"
+      type="button"
+      aria-checked={checked}
+      onClick={handleClick}
+      {...rest}
+    >
+      <CheckCircleIcon checked={checked} />
+      <div className={styles.labelWrapper}>
+        <span className={styles['bodyshort-medium']}>{label}</span>
+      </div>
+    </button>
+  );
+};
