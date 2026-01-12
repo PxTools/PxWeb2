4a7e794c9b0a5e950dfd0a778a39b104dc281b12
Sjur Sutterud Sagen <sjs@ssb.no>
2026-01-12 08:53:24 +0100

    feat: update SkipToContent and SkipToMain components to use getLanguagePath

--- packages/pxweb2/src/app/components/SkipToContent/SkipToContent.tsx
+++ packages/pxweb2/src/app/components/SkipToContent/SkipToContent.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import cl from 'clsx';
 import { useLocation, useSearchParams } from 'react-router';
+import { useTranslation } from 'react-i18next';
 
 import classes from './SkipToContent.module.scss';
 import { Link } from '@pxweb2/pxweb2-ui';
+import { getConfig } from '../../util/config/getConfig';
+import { getLanguagePath } from '../../util/language/getLanguagePath';
 
 export type SkipToContentProps = React.HTMLAttributes<HTMLDivElement> & {
   // Jump to
@@ -17,14 +20,26 @@ export type SkipToContentProps = React.HTMLAttributes<HTMLDivElement> & {
 
 export function SkipToContent(props: SkipToContentProps) {
   const { targetId, label, containerRef, ...rest } = props;
+  const { i18n } = useTranslation();
+  const config = getConfig();
   const location = useLocation().pathname;
   const [searchParams] = useSearchParams();
+
+  const basePath = getLanguagePath(
+    location,
+    i18n.language,
+    config.language.supportedLanguages,
+    config.language.defaultLanguage,
+    config.language.showDefaultLanguageInPath,
+    config.baseApplicationPath,
+    config.language.positionInPath,
+  );
 
   // build a single, URL-encoded query string from the search params
   const paramsString = searchParams.toString(); // URLSearchParams handles encoding
   const params = paramsString ? `?${paramsString}` : '';
   const hash = targetId ? `#${targetId}` : '';
-  const path = `${location}${params}${hash}`;
+  const path = `${basePath}${params}${hash}`;
 
   return (
     <div
