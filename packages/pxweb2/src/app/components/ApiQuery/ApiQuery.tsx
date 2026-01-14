060eced7fb31ea73b363f657effe4730542dea56
MikaelNordberg <mikael.nordberg@scb.se>
2026-01-14 11:35:52 +0100

    feat: Enhance ApiQuery component with description, links, selection, and code snippet

--- packages/pxweb2/src/app/components/ApiQuery/ApiQuery.tsx
+++ packages/pxweb2/src/app/components/ApiQuery/ApiQuery.tsx
@@ -1,34 +1,64 @@
 import cl from 'clsx';
-// import { useTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 
 import styles from './ApiQuery.module.scss';
-// import { CodeSnippet } from '@pxweb2/pxweb2-ui';
+import { BodyLong, Link, Chips, CodeSnippet } from '@pxweb2/pxweb2-ui';
 
 export interface ApiQueryProps {}
 
 export const ApiQuery: React.FC<ApiQueryProps> = () => {
-  // const { t } = useTranslation();
+  const { t } = useTranslation();
 
-  // const codeSnippetTranslations = {
-  //   copyButtonLabel: t('common.code_snippet.copy_button_label'),
-  //   copiedButtonLabel: t('common.code_snippet.copied_button_label'),
-  //   copyButtonTooltip: t('common.code_snippet.copy_button_tooltip'),
-  //   wrapCodeButtonLabel: t('common.code_snippet.wrap_code_button_label'),
-  //   unwrapCodeButtonLabel: t('common.code_snippet.unwrap_code_button_label'),
-  // };
+  const codeSnippetTranslations = {
+    copyButtonLabel: t('common.code_snippet.copy_button_label'),
+    copiedButtonLabel: t('common.code_snippet.copied_button_label'),
+    copyButtonTooltip: t('common.code_snippet.copy_button_tooltip'),
+    wrapCodeButtonLabel: t('common.code_snippet.wrap_code_button_label'),
+    unwrapCodeButtonLabel: t('common.code_snippet.unwrap_code_button_label'),
+  };
 
   return (
     <div className={cl(styles.apiQuery)}>
-      query
-      {/* <CodeSnippet title={temp} translations={codeSnippetTranslations}>
-        {`{
-        "query": [
+      <div className={cl(styles.descriptionAndLink)}>
+        <div className={cl(styles.descriptionWrapper)}>
+          <BodyLong size="medium">
+            API query description text. API query description text. API query
+            description text. API query description text. API query description
+            text. API query description text. API query description text. API
+            query description text.
+          </BodyLong>
+        </div>
+        <div className={cl(styles.linkWrapper)}>
+          <Link href="#" size="medium">
+            Read more about API
+          </Link>
+        </div>
+      </div>
+      <div className={cl(styles.selectWrapper)}>
+        <select name="cars" id="cars">
+          <option value="volvo">Volvo</option>
+          <option value="saab">Saab</option>
+          <option value="mercedes">Mercedes</option>
+          <option value="audi">Audi</option>
+        </select>
+      </div>
+      <div className={cl(styles.chipsWrapper)}>
+        <Chips>
+          <Chips.Toggle>region</Chips.Toggle>
+          <Chips.Toggle>year</Chips.Toggle>
+        </Chips>
+      </div>
+      <div className={cl(styles.codeSnippetWrapper)}>
+        <CodeSnippet title="GET URL" translations={codeSnippetTranslations}>
+          {`{
+            "query": [
           {
             "code": "region"
           }
         ],
       }`}
-      </CodeSnippet> */}
+        </CodeSnippet>
+      </div>
     </div>
   );
 };
