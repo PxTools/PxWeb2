5eb54cce08ae91317c3b85a6a97b167f9b33f068
Sjur Sutterud Sagen <sjs@ssb.no>
2026-01-16 13:35:25 +0100

    feat: Add language in path before basePath (#1076)
    Adds functionality for handling the language before the baseApplicationPath. This will enable links like:
    "/en/app/table/123".
    
    This updates the config with a new option "positionInPath", which defaults to "after".

--- packages/pxweb2/src/app/components/SkipToMain/SkipToMain.tsx
+++ packages/pxweb2/src/app/components/SkipToMain/SkipToMain.tsx
@@ -6,20 +6,32 @@ import { useLocation, useSearchParams } from 'react-router';
 import classes from './SkipToMain.module.scss';
 import { Link } from '@pxweb2/pxweb2-ui';
 import { getConfig } from '../../util/config/getConfig';
+import { getLanguagePath } from '../../util/language/getLanguagePath';
 
 export const SkipToMain = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
 >((props, ref) => {
-  const { t } = useTranslation();
-
-  const basePath = getConfig().baseApplicationPath;
+  const { t, i18n } = useTranslation();
+  const config = getConfig();
   const location = useLocation().pathname;
   const [searchParams] = useSearchParams();
+
+  const basePath = getLanguagePath(
+    location,
+    i18n.language,
+    config.language.supportedLanguages,
+    config.language.defaultLanguage,
+    config.language.showDefaultLanguageInPath,
+    config.baseApplicationPath,
+    config.language.positionInPath,
+  );
+
   // build a single, URL-encoded query string from the search params
   const paramsString = searchParams.toString(); // URLSearchParams handles encoding
   const params = paramsString ? `?${paramsString}` : '';
-  const path = basePath.slice(0, -1) + location + params + '#px-main-content';
+  const path = `${basePath}${params}#px-main-content`;
+
   return (
     <div
       ref={ref}
