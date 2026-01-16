890d4e7c66be2f7e5247c160da6593d4b964b930
MikaelNordberg <mikael.nordberg@scb.se>
2026-01-16 12:57:39 +0100

    feat: Refactor getApiQueryInfo function and implement useVariablesSelection for variable management

--- packages/pxweb2/src/app/util/apiQuery/apiQueryUtil.tsx
+++ packages/pxweb2/src/app/util/apiQuery/apiQueryUtil.tsx
@@ -1,4 +1,9 @@
-import type { SelectedVBValues } from '@pxweb2/pxweb2-ui';
+import {
+  VariableSelection,
+  VariablesSelection,
+} from '@pxweb2/pxweb2-api-client';
+import useVariables from '../../context/useVariables';
+// import type { SelectedVBValues } from '@pxweb2/pxweb2-ui';
 
 export type ApiQueryInfoType = {
   getUrl: string;
@@ -6,14 +11,39 @@ export type ApiQueryInfoType = {
   postBody: string;
 };
 
-export function getApiQueryInfo(
-  selectedVBValues: SelectedVBValues[],
-): ApiQueryInfoType {
-  console.log({ selectedVBValues });
+export function getApiQueryInfo(): ApiQueryInfoType {
   const apiQueryInfo: ApiQueryInfoType = {
     getUrl: 'https://api.pxweb2.test/getQueryExample',
     postUrl: 'https://api.pxweb2.test/postQueryExample',
     postBody: '{"query":[{"code":"region"}],}',
   };
   return apiQueryInfo;
+}
+
+export function useVariablesSelection(): VariablesSelection {
+  const variables = useVariables();
+
+  const selections: Array<VariableSelection> = [];
+
+  // Get selection from Selection provider
+  const ids = variables.getUniqueIds();
+  ids.forEach((id) => {
+    const selectedCodeList = variables.getSelectedCodelistById(id);
+    const selection: VariableSelection = {
+      variableCode: id,
+      valueCodes: variables.getSelectedValuesByIdSorted(id),
+    };
+
+    // Add selected codelist to selection if it exists
+    if (selectedCodeList) {
+      selection.codelist = selectedCodeList;
+    }
+
+    selections.push(selection);
+  });
+
+  const variablesSelection: VariablesSelection = {
+    selection: selections,
+  };
+  return variablesSelection;
 }
