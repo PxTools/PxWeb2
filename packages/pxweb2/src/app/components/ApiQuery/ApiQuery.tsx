dea8c87e3e44b0bf9fed596020b805b4cfad0def
MikaelNordberg <mikael.nordberg@scb.se>
2026-01-22 13:07:27 +0100

    fix: Update maxHeight of CodeSnippet to improve display

--- packages/pxweb2/src/app/components/ApiQuery/ApiQuery.tsx
+++ packages/pxweb2/src/app/components/ApiQuery/ApiQuery.tsx
@@ -112,7 +112,7 @@ export const ApiQuery: React.FC<ApiQueryProps> = () => {
           <div className={cl(styles.codeSnippetWrapper)}>
             <CodeSnippet
               highlight="json"
-              maxHeight="600px"
+              maxHeight="644px"
               title={t('presentation_page.side_menu.save.api.post_body_text')}
               translations={codeSnippetTranslations}
             >
