7e885a9216601c8d88124f68e4fc569297cbd0f7
Sjur Sutterud Sagen <sjs@ssb.no>
2026-01-29 08:35:32 +0100

    Remove redundant metadata checks

--- packages/pxweb2/src/app/components/TableInformation/Definitions/Metadata/Metadata.tsx
+++ packages/pxweb2/src/app/components/TableInformation/Definitions/Metadata/Metadata.tsx
@@ -16,59 +16,55 @@ export function Metadata({ variablesDefinitions }: MetadataProps) {
   }
 
   return (
-    <>
-      {variablesDefinitions && variablesDefinitions.length > 0 && (
-        <div className={cl(classes.metadataContainer)}>
-          <div className={cl(classes.metadataHeader)}>
-            <Heading level={'3'} size="small" spacing={true}>
-              {t(
-                'presentation_page.main_content.about_table.definitions.metadata.title',
-              )}
+    <div className={cl(classes.metadataContainer)}>
+      <div className={cl(classes.metadataHeader)}>
+        <Heading level={'3'} size="small" spacing={true}>
+          {t(
+            'presentation_page.main_content.about_table.definitions.metadata.title',
+          )}
+        </Heading>
+        <BodyLong size="medium">
+          {t(
+            'presentation_page.main_content.about_table.definitions.metadata.description',
+          )}
+        </BodyLong>
+      </div>
+
+      {variablesDefinitions.map((variable) => (
+        <div
+          key={variable.variableName}
+          className={cl(classes.variableLinkGroup)}
+        >
+          <div id={`sidesheet-metadata-${variable.variableName}`}>
+            <Heading size="xsmall" level="4" spacing={true}>
+              {variable.variableName}
             </Heading>
-            <BodyLong size="medium">
-              {t(
-                'presentation_page.main_content.about_table.definitions.metadata.description',
-              )}
-            </BodyLong>
           </div>
 
-          {variablesDefinitions.map((variable) => (
-            <div
-              key={variable.variableName}
-              className={cl(classes.variableLinkGroup)}
-            >
-              <div id={`sidesheet-metadata-${variable.variableName}`}>
-                <Heading size="xsmall" level="4" spacing={true}>
-                  {variable.variableName}
-                </Heading>
-              </div>
-
-              <ul
-                className={cl(classes.linkGroupWrapper)}
-                aria-labelledby={`sidesheet-metadata-${variable.variableName}`}
+          <ul
+            className={cl(classes.linkGroupWrapper)}
+            aria-labelledby={`sidesheet-metadata-${variable.variableName}`}
+          >
+            {variable.links.map((link, index) => (
+              <li
+                key={`${link.href}-${index}`}
+                className={cl(classes.linkWrapper)}
               >
-                {variable.links.map((link, index) => (
-                  <li
-                    key={`${link.href}-${index}`}
-                    className={cl(classes.linkWrapper)}
-                  >
-                    <Link
-                      href={link.href}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      icon="FileText"
-                      iconPosition="start"
-                      size="medium"
-                    >
-                      {link.label}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          ))}
+                <Link
+                  href={link.href}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  icon="FileText"
+                  iconPosition="start"
+                  size="medium"
+                >
+                  {link.label}
+                </Link>
+              </li>
+            ))}
+          </ul>
         </div>
-      )}
-    </>
+      ))}
+    </div>
   );
 }
