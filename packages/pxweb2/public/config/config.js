9fad02826d74b5047c526ac0cd2baf8da5cc2870
MikaelNordberg <mikael.nordberg@scb.se>
2026-02-17 15:53:42 +0100

    refactor: remove Swedish language option from minimum configuration

--- packages/pxweb2/public/config/config.js
+++ packages/pxweb2/public/config/config.js
@@ -46,7 +46,6 @@ globalThis.PxWeb2Config = {
   language: {
     supportedLanguages: [
       { shorthand: 'en', languageName: 'English' },
-      { shorthand: 'sv', languageName: 'Svenska' },
     ],
     defaultLanguage: 'en',
     fallbackLanguage: 'en',
