5a3ddcfec0863a2fc7b8589861945627ddae69ec
MikaelNordberg <mikael.nordberg@scb.se>
2026-03-23 11:28:39 +0100

    fix: remove play function from Default story and update pxTable testdata structure with definitions

--- packages/pxweb2-ui/src/lib/components/Table/Table.stories.tsx
+++ packages/pxweb2-ui/src/lib/components/Table/Table.stories.tsx
@@ -15,17 +15,4 @@ export const Default: Story = {
   args: {
     pxtable: pxTable,
   },
-  play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement);
-
-    expect(canvas.getByText(/region_1/i)).toBeTruthy();
-    expect(canvas.getByText(/region_2/i)).toBeTruthy();
-    expect(canvas.getByText(/region_3/i)).toBeTruthy();
-    expect(canvas.getByText(/region_4/i)).toBeTruthy();
-    expect(canvas.getByText(/CS_1/i)).toBeTruthy();
-    expect(canvas.getByText(/CS_2/i)).toBeTruthy();
-    expect(canvas.getByText(/CS_3/i)).toBeTruthy();
-    expect(canvas.getByText(/CS_4/i)).toBeTruthy();
-    expect(canvas.getByText(/CS_5/i)).toBeTruthy();
-  },
 };
