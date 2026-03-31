319706ec5fed884e8fb7b213cbaa1319d14c5883
Kent Mossbäck <kent.mossback@scb.se>
2026-03-31 13:04:45 +0200

    style: format SkipToToolsMenu component for improved readability

--- packages/pxweb2/src/app/components/SkipToMain/SkipToToolsMenu.tsx
+++ packages/pxweb2/src/app/components/SkipToMain/SkipToToolsMenu.tsx
@@ -3,17 +3,17 @@ import React from 'react';
 import { SkipLink } from './SkipLink';
 
 export const SkipToToolsMenu = React.forwardRef<
-	HTMLDivElement,
-	React.HTMLAttributes<HTMLDivElement>
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
 >((props, ref) => {
-	return (
-		<SkipLink
-			ref={ref}
-			targetId="px-table-viewer-outer-container"
-			translationKey="common.skip_to_toolsmenu"
-			{...props}
-		/>
-	);
+  return (
+    <SkipLink
+      ref={ref}
+      targetId="px-table-viewer-outer-container"
+      translationKey="common.skip_to_toolsmenu"
+      {...props}
+    />
+  );
 });
 
 SkipToToolsMenu.displayName = 'SkipToToolsMenu';
