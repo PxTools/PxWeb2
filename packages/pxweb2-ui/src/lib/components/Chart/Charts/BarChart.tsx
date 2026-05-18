bd832af4b919aad895df2ae5633282cc45805156
MikaelNordberg <mikael.nordberg@scb.se>
2026-05-18 16:33:39 +0200

    Add tooltip configuration to BarChart for enhanced interactivity

--- packages/pxweb2-ui/src/lib/components/Chart/Charts/BarChart.tsx
+++ packages/pxweb2-ui/src/lib/components/Chart/Charts/BarChart.tsx
@@ -29,6 +29,12 @@ export function BarChart({
       legend: {
         height: 40 * dataset.series.length, // increase legend height based on number of series to prevent overlap with x-axis labels
       },
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+          type: 'shadow',
+        },
+      },
     };
   }, [dataset, isHorizontal, colors]);
 
