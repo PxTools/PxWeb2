2b4c3f0b4aea39ea9fca28e526c7896af82cb67a
MikaelNordberg <mikael.nordberg@scb.se>
2026-05-19 11:30:03 +0200

    Refactor: update import path for pxTable to use Test directory

--- packages/pxweb2-ui/src/lib/components/Table/Table.stories.tsx
+++ packages/pxweb2-ui/src/lib/components/Table/Table.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
 
 import { Table } from './Table';
-import { pxTable } from './testData';
+import { pxTable } from './Test/testData';
 
 const meta: Meta<typeof Table> = {
   component: Table,
