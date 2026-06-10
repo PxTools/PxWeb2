19b4e610488eb10c4f7696ea21c77c46237cfcc7
MikaelNordberg <mikael.nordberg@scb.se>
2026-06-10 13:45:33 +0200

    test: wrap DrawerView component in MemoryRouter for routing context in tests

--- packages/pxweb2/src/app/components/NavigationDrawer/Drawers/DrawerView.spec.tsx
+++ packages/pxweb2/src/app/components/NavigationDrawer/Drawers/DrawerView.spec.tsx
@@ -1,12 +1,17 @@
 import { describe, it, expect } from 'vitest';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/vitest';
+import { MemoryRouter } from 'react-router';
 
 import { DrawerView } from './DrawerView';
 
 describe('DrawerView', () => {
   it('renders successfully', () => {
-    const element = render(<DrawerView />);
+    const element = render(
+      <MemoryRouter>
+        <DrawerView />
+      </MemoryRouter>,
+    );
 
     expect(element).toBeTruthy();
   });
