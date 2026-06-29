7d9b1f316180192d2f8b671079ab8818225d9f96
Sjur Sutterud Sagen <sjs@ssb.no>
2026-06-29 14:21:11 +0200

    Upgrade react-router from v7 to v8
    This updates to the new, v8, major version of React Router. There were
    no breaking changes that affected us, but we fixed a minor import issue
    in main.tsx. And now we import the RouterProvider from the correct
    sub-package.
    
    Co-authored-by: Mikael Nordberg <54406826+MikaelNordberg@users.noreply.github.com>

--- packages/pxweb2/src/main.tsx
+++ packages/pxweb2/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router';
+import { createBrowserRouter } from 'react-router';
+import { RouterProvider } from 'react-router/dom';
 
 import './i18n/config';
 import { validateConfig } from './app/util/validate';
