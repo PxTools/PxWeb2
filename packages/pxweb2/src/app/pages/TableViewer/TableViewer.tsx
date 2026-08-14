0b629be8a568dd29d3f120960c0c37144fe0d462
PerIngeVaaje <per.inge.vaaje@ssb.no>
2026-08-14 10:45:35 +0200

    remove commented out code

--- packages/pxweb2/src/app/pages/TableViewer/TableViewer.tsx
+++ packages/pxweb2/src/app/pages/TableViewer/TableViewer.tsx
@@ -276,7 +276,6 @@ export function TableViewer() {
                 <Footer enableWindowScroll />
               </div>
             </div>
-            {/* </div> */}
           </div>
         </div>
       </div>
