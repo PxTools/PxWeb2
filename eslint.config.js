d3cb1a0c2c8bf1d9362448ce891c0f2d28efbc1a
Sjur Sutterud Sagen <sjs@ssb.no>
2026-09-03 15:24:23 +0200

    Fix eslint 10 compatability
    This removes and cleans up our Eslint config, by removing unused
    lint-packages, and by removing the problematic eslint-plugin-react.
    That package still does not support Eslint 10, and Eslint 9 has been
    EOL for almost a month.
    
    Lint output is the same as before, though we have lost some rules. We
    can reevalute the package when it supports Eslint 10. Or go with an
    alternative.
    
    The biggest change for everyday development might be the removale of
    the unused imports rules/package.

--- eslint.config.js
+++ eslint.config.js
@@ -3,9 +3,7 @@ import { defineConfig } from 'eslint/config';
 import storybook from 'eslint-plugin-storybook';
 import tseslint from '@typescript-eslint/eslint-plugin';
 import tsParser from '@typescript-eslint/parser';
-import reactPlugin from 'eslint-plugin-react';
 import reactHooksPlugin from 'eslint-plugin-react-hooks';
-import unusedImportsPlugin from 'eslint-plugin-unused-imports';
 
 export default defineConfig([
   {
@@ -23,9 +21,7 @@ export default defineConfig([
     files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
     plugins: {
       '@typescript-eslint': tseslint,
-      react: reactPlugin,
       'react-hooks': reactHooksPlugin,
-      'unused-imports': unusedImportsPlugin,
     },
     languageOptions: {
       parser: tsParser,
@@ -49,23 +45,12 @@ export default defineConfig([
       '@typescript-eslint/no-unused-vars': 'error',
 
       // React rules
-      'react/function-component-definition': [
-        'error',
-        {
-          namedComponents: ['function-declaration', 'arrow-function'],
-          unnamedComponents: 'arrow-function',
-        },
-      ],
-      'react/jsx-uses-react': 'error',
-      'react/jsx-uses-vars': 'error',
-      'react/react-in-jsx-scope': 'off',
       'react-hooks/rules-of-hooks': 'error',
       'react-hooks/exhaustive-deps': 'warn',
 
       // General rules
       curly: ['error', 'all'], // Enforce curly braces for all control statements
       'no-extra-semi': 'off', // Turned off in favor of @typescript-eslint/no-extra-semi
-      'unused-imports/no-unused-imports': 'error',
     },
   },
   ...storybook.configs['flat/recommended'],
