636db27e1c506e19c27770b445d651ddf6b5e822
MikaelNordberg <mikael.nordberg@scb.se>
2026-09-25 10:17:09 +0200

    Refactor: Removed unneccesary code

--- packages/pxweb2-ui/src/lib/components/Chart/LineChart/LineChart.tsx
+++ packages/pxweb2-ui/src/lib/components/Chart/LineChart/LineChart.tsx
@@ -228,9 +228,6 @@ export function LineChart({
       legend: {
         data: visibleLegendData,
         orient: 'horizontal',
-        left: 0,
-        right: 0,
-        bottom: 0,
         textStyle: {
           overflow: 'break',
         },
